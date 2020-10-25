1b7868475af68f4ac369d1d18bf24960bc24c12e
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-10-25 17:37:09 +0000

    Added additional reference and import to 'DscResource.Common', helper PSModule

--- source/Modules/AzureDevOpsDsc.Common/AzureDevOpsDsc.Common.psm1
+++ source/Modules/AzureDevOpsDsc.Common/AzureDevOpsDsc.Common.psm1
@@ -1,5 +1,10 @@
+# Setup/Import 'DscResource.Common' helper module
+$script:resourceHelperModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Modules\DscResource.Common'
+Import-Module -Name $script:resourceHelperModulePath
+
 
 $script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
+
 
 # Obtain all functions within PSModule
 $functionSubDirectoryPaths = @(
