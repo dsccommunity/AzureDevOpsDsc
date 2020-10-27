6f8486593ca66e5dc58d483687531ed169dee372
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-10-27 19:57:36 +0000

    Added 'CommonTestHelper.psm1' with 'Test-BuildCategory' and 'Test-ContinuousIntegrationTaskCategory' functions (taken from 'SqlServerDsc' project/repository.

---
+++ tests/Unit/Modules/TestHelpers/CommonTestHelper.psm1
@@ -0,0 +1,106 @@
+
+
+<#
+    .SYNOPSIS
+        Returns $true if the the environment variable APPVEYOR is set to $true,
+        and the environment variable CONFIGURATION is set to the value passed
+        in the parameter Type.
+
+    .PARAMETER Name
+        Name of the test script that is called. Default value is the name of the
+        calling script.
+
+    .PARAMETER Type
+        Type of tests in the test file. Can be set to Unit or Integration.
+
+    .PARAMETER Category
+        Optional. One or more categories to check if they are set in
+        $env:CONFIGURATION. If this are not set, the parameter Type
+        is used as category.
+#>
+function Test-BuildCategory
+{
+    [OutputType([System.Boolean])]
+    [CmdletBinding()]
+    param
+    (
+        [Parameter()]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $Name = $MyInvocation.PSCommandPath.Split('\')[-1],
+
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('Unit', 'Integration')]
+        [System.String]
+        $Type,
+
+        [Parameter()]
+        [System.String[]]
+        $Category
+    )
+
+    # Support using only the Type parameter as category names.
+    if (-not $Category)
+    {
+        $Category = @($Type)
+    }
+
+    $result = $true
+
+    if ($Type -eq 'Integration' -and -not $env:CI -eq $true)
+    {
+        Write-Warning -Message ('{1} test for {0} will be skipped unless $env:CI is set to $true' -f $Name, $Type)
+        $result = $false
+    }
+
+    <#
+        If running in CI then check if it should run in the
+        current category set in $env:CONFIGURATION.
+    #>
+    if ($env:CI -eq $true -and -not (Test-ContinuousIntegrationTaskCategory -Category $Category))
+    {
+        Write-Verbose -Message ('{1} tests in {0} will be skipped unless $env:CONFIGURATION is set to ''{1}''.' -f $Name, ($Category -join ''', or ''')) -Verbose
+        $result = $false
+    }
+
+    return $result
+}
+
+<#
+    .SYNOPSIS
+        Returns $true if the the environment variable APPVEYOR is set to $true,
+        and the environment variable CONFIGURATION is set to the value passed
+        in the parameter Type.
+
+    .PARAMETER Category
+        One or more categories to check if they are set in $env:CONFIGURATION.
+#>
+function Test-ContinuousIntegrationTaskCategory
+{
+    [OutputType([System.Boolean])]
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String[]]
+        $Category
+    )
+
+    $result = $false
+
+    if ($env:CI -eq $true -and $env:CONFIGURATION -in $Category)
+    {
+        $result = $true
+    }
+
+    return $result
+}
+
+<#
+    .SYNOPSIS
+        Waits for LCM to become idle.
+
+    .NOTES
+        Used in integration test where integration tests run to quickly before
+        LCM have time to cool down.
+#>
