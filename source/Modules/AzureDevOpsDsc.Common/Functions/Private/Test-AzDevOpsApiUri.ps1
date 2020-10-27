9020eb220163f89e4993aa0cc32bec6260ab49fe
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-10-27 15:57:38 +0000

    Added a number of initial 'Test-' functions to validate other function, input parameters.

---
+++ source/Modules/AzureDevOpsDsc.Common/Functions/Private/Test-AzDevOpsApiUri.ps1
@@ -0,0 +1,48 @@
+<#
+    .SYNOPSIS
+        Peforms test on a provided URI of the Azure DevOps API to provide a
+        boolean ($true or $false) return value. Returns $true if the test is successful.
+
+        NOTE: Use of the '-IsValid' switch is required.
+
+    .PARAMETER ApiUri
+        The URI of the Azure DevOps API to be tested/validated.
+
+    .PARAMETER IsValid
+        Use of this switch will validate the format of the URI of the Azure DevOps API
+        rather than the existence/presence of the API/URI itself.
+
+        Failure to use this switch will throw an exception.
+
+    .EXAMPLE
+        Test-AzDevOpsApiUri -ApiUri 'YourApiUriHere' -IsValid
+
+        Returns $true if the URI of the Azure DevOps API provided is of a valid format.
+        Returns $false if it is not.
+#>
+function Test-AzDevOpsApiUri
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [System.String]
+        $ApiUri,
+
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.SwitchParameter]
+        $IsValid
+    )
+
+    if (!$IsValid)
+    {
+        $errorMessage = $script:localizedData.MandatoryIsValidSwitchNotUsed -f $MyInvocation.MyCommand
+        New-InvalidOperationException -Message $errorMessage
+    }
+
+    # TODO:
+    #  - Validate it's a valid URL
+
+    return $true
+}
