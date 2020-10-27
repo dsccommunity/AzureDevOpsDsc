e5b06d7ef736db5caa9cbc93a38420ca475d2c8b
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-10-27 16:13:14 +0000

    Added empty, 'AzureDevOpsDsc.Server' PSModule.

---
+++ source/Modules/AzureDevOpsDsc.Server/AzureDevOpsDsc.Server.psm1
@@ -0,0 +1,32 @@
+# Setup/Import 'DscResource.Server' helper module
+$script:resourceHelperModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Modules\DscResource.Server'
+Import-Module -Name $script:resourceHelperModulePath
+
+
+$script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
+
+
+# Obtain all functions within PSModule
+$functionSubDirectoryPaths = @(
+    "$PSScriptRoot\Functions\Public"
+    "$PSScriptRoot\Functions\Private"
+)
+$functions = Get-ChildItem -Path $functionSubDirectoryPaths -Recurse -Include "*.ps1"
+
+
+# Loop through all PSModule functions and import/dot-source them (and export them if 'Public')
+ForEach ($function in $functions)
+{
+    Write-Verbose "Dot-sourcing '$($function.FullName)'..."
+    . (
+        [ScriptBlock]::Create(
+            [Io.File]::ReadAllText($($function.FullName))
+        )
+    )
+
+    if ($function.FullName -ilike "$PSScriptRoot\Functions\Public\*")
+    {
+        Write-Verbose "Exporting '$($function.BaseName)'..."
+        Export-ModuleMember -Function $($function.BaseName)
+    }
+}
---
+++ source/Modules/AzureDevOpsDsc.Server/en-US/AzureDevOpsDsc.Common.strings.psd1
@@ -0,0 +1,5 @@
+# Localized resources for helper module AzureDevOpsDsc.Server.
+
+ConvertFrom-StringData @'
+
+'@
