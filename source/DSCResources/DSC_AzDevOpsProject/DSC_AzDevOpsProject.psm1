0b70519bb2d57ca129b80792aa4eaa12d310292e
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-10-27 21:37:07 +0000

    Moved 'DSCClassResources' back into 'DSCResources'.

--- source/DSCResources/DSC_AzDevOpsProject/DSC_AzDevOpsProject.psm1
+++ source/DSCResources/DSC_AzDevOpsProject/DSC_AzDevOpsProject.psm1
@@ -1,12 +1,12 @@
 $script:azureDevOpsDscCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Modules\AzureDevOpsDsc.Common'
 $script:azureDevOpsDscServerModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Modules\AzureDevOpsDsc.Server'
 $script:azureDevOpsDscServicesModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Modules\AzureDevOpsDsc.Services'
-$script:dscResourceCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Modules\DscResource.Common'
+#$script:dscResourceCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Modules\DscResource.Common'
 
 Import-Module -Name $script:azureDevOpsDscCommonModulePath
 Import-Module -Name $script:azureDevOpsDscServerModulePath
 Import-Module -Name $script:azureDevOpsDscServicesModulePath
-Import-Module -Name $script:dscResourceCommonModulePath
+#Import-Module -Name $script:dscResourceCommonModulePath
 
 $script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
 
@@ -18,24 +18,45 @@ enum Ensure
 }
 
 
-[DscResource('AzDevOpsProject')]
+[DscResource()]
 class DSC_AzDevOpsProject
 {
+
+    [DscProperty()]
+    [Alias('Uri')]
+    [string]$ApiUri
+
+    [DscProperty()]
+    [Alias('PersonalAccessToken')]
+    [string]$Pat
+
 
     [DscProperty(Key,Mandatory)]
     [Alias('Name')]
     [string]$ProjectName
 
-    [DscProperty(Mandatory)]
+    [DscProperty()]
     [Ensure]$Ensure
 
+    [DscProperty()]
     [Alias('Description')]
     [string]$ProjectDescription
 
 
+
     [DSC_AzDevOpsProject] Get()
     {
-        return this
+        $parameterSet = @{
+            ApiUri      = $this.ApiUri
+            Pat         = $this.Pat
+            ProjectName = $this.ProjectName
+        }
+
+        return [DSC_AzDevOpsProject]::new() #|
+                    #Add-Member -NotePropertyName 'ApiUri' -NotePropertyValue $this.ApiUri -Force -PassThru |
+                    #Add-Member -NotePropertyName 'Pat' -NotePropertyValue $this.Pat -Force -PassThru |
+                    #Add-Member -NotePropertyName 'ProjectName' -NotePropertyValue $this.ProjectName -Force -PassThru
+
     }
 
 
