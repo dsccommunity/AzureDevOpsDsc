a7107a4d999e0af9fa33f62cbc8326029166bfdf
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-10-27 21:35:09 +0000

    Commented out import of 'DscResource.Common' in embbeded modules.

--- source/Modules/AzureDevOpsDsc.Services/AzureDevOpsDsc.Services.psm1
+++ source/Modules/AzureDevOpsDsc.Services/AzureDevOpsDsc.Services.psm1
@@ -1,6 +1,6 @@
 # Setup/Import 'DscResource.Services' helper module
-$script:resourceHelperModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Modules\DscResource.Services'
-Import-Module -Name $script:resourceHelperModulePath
+#$script:resourceHelperModulePath = Join-Path -Path $PSScriptRoot -ChildPath '..\..\Modules\DscResource.Services'
+#Import-Module -Name $script:resourceHelperModulePath
 
 
 $script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
