06e0a6f9c8822eff3cb00b0b70a1788595471f7f
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-10-28 20:38:33 +0000

    Seperated out module tests into 'AzureDevOpsDsc.Common.TestInitialization.ps1' script and 'Get-AzDevOpsServicesUri.Tests.ps1'

---
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/AzureDevOpsDsc.Common.TestInitialization.ps1
@@ -0,0 +1,28 @@
+<#
+    .SYNOPSIS
+        Automated unit test for helper functions in module AzureDevOpsDsc.Common.
+#>
+
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+
+if (-not (Test-BuildCategory -Type 'Unit'))
+{
+    return
+}
+
+$script:dscModuleName = 'AzureDevOpsDsc'
+$script:subModuleName = 'AzureDevOpsDsc.Common'
+
+#region HEADER
+Remove-Module -Name $script:subModuleName -Force -ErrorAction 'SilentlyContinue'
+
+$script:parentModule = Get-Module -Name $script:dscModuleName -ListAvailable | Select-Object -First 1
+$script:subModulesFolder = Join-Path -Path $script:parentModule.ModuleBase -ChildPath 'Modules'
+
+$script:subModulePath = Join-Path -Path $script:subModulesFolder -ChildPath $script:subModuleName
+
+Import-Module -Name $script:subModulePath -Force -ErrorAction 'Stop'
+#endregion HEADER
+
+# Loading mocked classes
+#Add-Type -Path (Join-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Stubs') -ChildPath 'SomeExampleMockedClass.cs')
--- tests/Unit/Modules/AzureDevOpsDsc.Common/Functions/Private/Get-AzDevOpsServicesUri.Tests.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/Functions/Private/Get-AzDevOpsServicesUri.Tests.ps1
@@ -1,31 +1,7 @@
-<#
-    .SYNOPSIS
-        Automated unit test for helper functions in module AzureDevOpsDsc.Common.
-#>
 
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+# Initialize tests for module function
+. $PSScriptRoot\..\..\AzureDevOpsDsc.Common.TestInitialization.ps1
 
-if (-not (Test-BuildCategory -Type 'Unit'))
-{
-    return
-}
-
-$script:dscModuleName = 'AzureDevOpsDsc'
-$script:subModuleName = 'AzureDevOpsDsc.Common'
-
-#region HEADER
-Remove-Module -Name $script:subModuleName -Force -ErrorAction 'SilentlyContinue'
-
-$script:parentModule = Get-Module -Name $script:dscModuleName -ListAvailable | Select-Object -First 1
-$script:subModulesFolder = Join-Path -Path $script:parentModule.ModuleBase -ChildPath 'Modules'
-
-$script:subModulePath = Join-Path -Path $script:subModulesFolder -ChildPath $script:subModuleName
-
-Import-Module -Name $script:subModulePath -Force -ErrorAction 'Stop'
-#endregion HEADER
-
-# Loading mocked classes
-#Add-Type -Path (Join-Path -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Stubs') -ChildPath 'SomeExampleMockedClass.cs')
 
 InModuleScope $script:subModuleName {
     Describe 'AzureDevOpsDsc.Common\Get-AzDevOpsServicesUri' -Tag 'GetAzDevOpsServicesUri' {
