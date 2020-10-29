144c65468ec95435cb3740905bf070889570a241
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-10-29 23:34:00 +0000

    Added examples for 'DSC_AzDevOpsProject'

---
+++ source/Examples/Resources/DSC_AzDevOpsProject/2-RenameProject.ps1
@@ -0,0 +1,41 @@
+
+<#
+    .DESCRIPTION
+        This example shows how to ensure that an Azure DevOps project
+        with a ProjectId of '1aeda1ef-a16d-4118-8817-d2f85d4f05d1' can be
+        renamed to 'A New Test Project Name'.
+#>
+
+Configuration Example
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Url,
+
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Pat
+    )
+
+    Import-DscResource -ModuleName 'AzureDevOpsDsc'
+
+    node localhost
+    {
+        DSC_AzDevOpsProject 'RenameProject'
+        {
+            Ensure               = 'Present'
+
+            ApiUri               = $ApiUri
+            Pat                  = $Pat
+
+            ProjectId            = '1aeda1ef-a16d-4118-8817-d2f85d4f05d1'  # Example ProjectId (NOTE: This is mandatory for renaming a project)
+            ProjectName          = 'A New Test Project Name'
+            ProjectDescription   = 'A test project'
+
+            #PsDscRunAsCredential = $SqlAdministratorCredential
+        }
+
+    }
+}
