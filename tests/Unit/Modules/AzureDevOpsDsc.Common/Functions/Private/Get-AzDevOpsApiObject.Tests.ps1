13eba7cd424448a09fa7d94a74175e5f8028d587
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-02 18:01:17 +0000

    Updated tests for 'Get-AzDevOpsApiObject' to use 'Get-TestCase' helper method

--- tests/Unit/Modules/AzureDevOpsDsc.Common/Functions/Private/Get-AzDevOpsApiObject.Tests.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/Functions/Private/Get-AzDevOpsApiObject.Tests.ps1
@@ -9,121 +9,62 @@ InModuleScope $script:subModuleName {
 
         Context 'When called with valid parameters' {
             BeforeAll {
-                Mock Invoke-RestMethod {
-                    $response = @'
+
+                [string]$nonPresentObjectId = '114bff8d-6169-45cf-b085-fe121267e7aa'
+
+                Mock Invoke-RestMethod -Verifiable {
+
+                    $response = @"
                     {
                         "count": 2,
                         "value": [
-                          {
-                            "id": "8d4bff8d-6169-45cf-b085-fe12ad67e76b",
-                            "name": "Test Project 1",
-                            "description": "Test Project Description 1",
-                            "url": "https://dev.azure.com/fabrikam/_apis/projects/8d4bff8d-6169-45cf-b085-fe12ad67e76b",
-                            "state": "wellFormed"
-                          },
-                          {
-                            "id": "485ab935-7de3-4894-ba6f-ed418d4eda92",
-                            "name": "Test Project 2",
-                            "description": "Test Project Description 2",
-                            "url": "https://dev.azure.com/fabrikam/_apis/projects/485ab935-7de3-4894-ba6f-ed418d4eda92",
-                            "state": "wellFormed"
-                          }
+                            {
+                                "id": "$ObjectId",
+                                "name": "Test Project 1",
+                                "description": "Test Project Description 1",
+                                "url": "https://dev.azure.com/fabrikam/_apis/projects/$ObjectId",
+                                "state": "wellFormed"
+                            },
+                            {
+                                "id": "8d4bff8d-6169-45cf-b085-fe12ad67e76b",
+                                "name": "Test Project 2",
+                                "description": "Test Project Description 2",
+                                "url": "https://dev.azure.com/fabrikam/_apis/projects/8d4bff8d-6169-45cf-b085-fe12ad67e76b",
+                                "state": "wellFormed"
+                            }
                         ]
-                      }
-'@ | ConvertFrom-Json
+                    }
+"@  | ConvertFrom-Json
 
-                    if ([string]::IsNullOrWhiteSpace($ObjectId))
+                    if (![string]::IsNullOrWhiteSpace($ObjectId))
                     {
-                        $response = $response.Value
+                        $response = $response.value |
+                            Where-Object { $_.id -eq $ObjectId} |
+                            Where-Object { $_.id -ne $nonPresentObjectId}
                     }
-                    else {
-                        $response = $response.Value |
-                            Where-Object { $_.id -eq $ObjectId }
-                    }
+
+                    #if ([string]::IsNullOrWhiteSpace($ObjectId))
+                    #{
+                    #    $response = $response.Value
+                    #}
+                    #else {
+                    #    $response = $response.Value |
+                    #        Where-Object { $_.id -eq $ObjectId }
+                    #}
 
                     return $response
                 }
             }
 
-            $testCasesValidApiUris = @(
-                @{
-                    ApiUri = 'http://someuri.api/_apis/' },
-                @{
-                    ApiUri = 'https://someuri.api/_apis/' }
-            )
+            $testCasesValidApiUris = Get-TestCase -ScopeName 'ApiUri' -TestCaseName 'Valid'
+            $testCasesValidPats = Get-TestCase -ScopeName 'Pat' -TestCaseName 'Valid'
+            $testCasesValidApiUriPatCombined = Join-TestCaseArray -Expand -TestCases $testCasesValidApiUris, $testCasesValidPats
 
-            $testCasesValidPats = @(
-                @{
-                    Pat = '1234567890123456789012345678901234567890123456789012' },
-                @{
-                    Pat = '0987654321098765432109876543210987654321098765432109' },
-                @{
-                    Pat = '0913uhuh3wedwndfwsni2242msfwneu254uhufs009oosfmikm34' }
-            )
+            $testCasesValidObjectNames = Get-TestCase -ScopeName 'ObjectName' -TestCaseName 'Valid'
+            $testCasesValidApiUriPatObjectNameCombined = Join-TestCaseArray -Expand -TestCases $testCasesValidApiUriPatCombined, $testCasesValidObjectNames
 
-            $testCasesValidApiUriPatCombined = $testCasesValidApiUris | ForEach-Object {
-
-                $apiUri = $_.ApiUri
-                $testCasesValidPats | ForEach-Object {
-
-                    $pat = $_.Pat
-                    return @{
-                        ApiUri = $apiUri
-                        Pat = $pat
-                    }
-                }
-
-            }
-
-
-            $testCasesValidObjectNames = @(
-                @{
-                    ObjectName = 'Operation' },
-                @{
-                    ObjectName = 'Project' }
-            )
-
-            $testCasesValidObjectIds = @( # Note: Same as mock for 'Invoke-RestMethod'
-                @{
-                    ObjectId = '8d4bff8d-6169-45cf-b085-fe12ad67e76b' },
-                @{
-                    ObjectId = '485ab935-7de3-4894-ba6f-ed418d4eda92' }
-            )
-
-            $testCasesValidApiUriPatObjectNameCombined = $testCasesValidApiUriPatCombined | ForEach-Object {
-
-                $apiUri = $_.ApiUri
-                $pat = $_.Pat
-                $testCasesValidObjectNames | ForEach-Object {
-
-                    $objectName = $_.ObjectName
-                    return @{
-                        ApiUri = $apiUri
-                        Pat = $pat
-                        ObjectName = $objectName
-                    }
-                }
-
-            }
-
-            $testCasesValidApiUriPatObjectNameObjectIdCombined = $testCasesValidApiUriPatObjectNameCombined | ForEach-Object {
-
-                $apiUri = $_.ApiUri
-                $pat = $_.Pat
-                $objectName = $_.ObjectName
-                $testCasesValidObjectIds | ForEach-Object {
-
-                    $objectId = $_.ObjectId
-                    return @{
-                        ApiUri = $apiUri
-                        Pat = $pat
-                        ObjectName = $objectName
-                        ObjectId = $objectId
-                    }
-                }
-
-            }
-
+            $testCasesValidObjectIds = Get-TestCase -ScopeName 'ObjectId' -TestCaseName 'Valid'
+            $testCasesValidApiUriPatObjectNameObjectIdCombined = Join-TestCaseArray -Expand -TestCases $testCasesValidApiUriPatObjectNameCombined, $testCasesValidObjectIds
 
             Context 'When called with no "ObjectId" parameter' {
 
@@ -187,7 +128,7 @@ InModuleScope $script:subModuleName {
                     It 'Should return $null - "<ApiUri>", "<Pat>", "<ObjectName>"' -TestCases $testCasesValidApiUriPatObjectNameCombined {
                         param ([string]$ApiUri, [string]$Pat, [string]$ObjectName)
 
-                        $result = Get-AzDevOpsApiObject -ApiUri $ApiUri -Pat $Pat -ObjectName $ObjectName -ObjectId '114bff8d-6169-45cf-b085-fe121267e7aa' # Non-present "ObjectId"
+                        $result = Get-AzDevOpsApiObject -ApiUri $ApiUri -Pat $Pat -ObjectName $ObjectName -ObjectId $nonPresentObjectId # Non-present "ObjectId"
                         $result | Should -Be $null
                     }
                 }
@@ -204,27 +145,8 @@ InModuleScope $script:subModuleName {
 
             Context 'When called with invalid "Pat" parameter' {
 
-                $testCasesEmptyPats = @(
-                    @{
-                        ApiUri = $null },
-                    @{
-                        ApiUri = '' }
-                )
-
-                $testCasesInvalidPats = @(
-                    @{
-                        Pat = $null },
-                    @{
-                        Pat = '' }
-                    @{
-                        Pat = ' ' },
-                    @{
-                        Pat = 'a 1' },
-                    @{
-                        Pat = '0913uhuh3wedwnd4wsni2242msfwn4u254uhufs009oosfmikm3' },
-                    @{
-                        Pat = '0913uhuh3wedwnd4wsni2242msfwn4u254uhufs009oosfmikm34x' }
-                )
+                $testCasesEmptyPats = Get-TestCase -ScopeName 'Pat' -TestCaseName 'Empty'
+                $testCasesInvalidPats = Get-TestCase -ScopeName 'Pat' -TestCaseName 'Invalid'
 
                 Context 'When called without "ApiUri" parameter' {
                     It "Should throw - '<Pat>'" -TestCases $testCasesInvalidPats {
@@ -258,43 +180,8 @@ InModuleScope $script:subModuleName {
 
             Context 'When called with invalid "ApiUri" parameter' {
 
-                $testCasesEmptyApiUris = @(
-                    @{
-                        ApiUri = $null },
-                    @{
-                        ApiUri = '' }
-                )
-
-                $testCasesInvalidApiUris = @(
-                    @{
-                        ApiUri = ' ' },
-                    @{
-                        ApiUri = 'a 1' },
-
-                    # Incorrect prefixes
-                    @{
-                        ApiUri = 'ftp://someuri.api/_apis/' },
-                    @{
-                        ApiUri = 'someuri.api/_apis/' },
-
-                    # Missing trailing '/' (after http(s))
-                    @{
-                        ApiUri = 'http:/someuri.api/_apis/' },
-                    @{
-                        ApiUri = 'https:/someuri.api/_apis/' },
-
-                    # Missing trailing '/'
-                    @{
-                        ApiUri = 'http://someuri.api/_apis' },
-                    @{
-                        ApiUri = 'https://someuri.api/_apis' },
-
-                    # Missing trailing '/_apis/'
-                    @{
-                        ApiUri = 'http://someuri.api/' },
-                    @{
-                        ApiUri = 'https://someuri.api/' }
-                )
+                $testCasesEmptyApiUris = Get-TestCase -ScopeName 'ApiUri' -TestCaseName 'Empty'
+                $testCasesInvalidApiUris = Get-TestCase -ScopeName 'ApiUri' -TestCaseName 'Invalid'
 
                 Context 'When called without "Pat" parameter' {
                     It "Should throw - '<ApiUri>'" -TestCases $testCasesInvalidApiUris {
