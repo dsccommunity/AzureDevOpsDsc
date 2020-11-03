358d2aef9765fbe85d680d506fec89756f86cb8f
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-03 14:19:10 +0000

    Added 'Get-AzDevOpsApiUriAreaName' function

---
+++ source/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Get-AzDevOpsApiUriAreaName.ps1
@@ -0,0 +1,40 @@
+<#
+    .SYNOPSIS
+        Returns an array of all the Azure DevOps API, URI-specific, 'Area' names that can used/managed.
+
+    .EXAMPLE
+        Get-AzDevOpsApiUriAreaName
+
+        Returns all the names of the URI-specific, area names that can be used/managed in the Azure DevOps API.
+
+    .EXAMPLE
+        Get-AzDevOpsApiUriAreaName -ResourceName 'YourResourceNameHere'
+
+        Returns the URI-specific, resource name that can be used/managed in the Azure DevOps API for the given
+        'ResourceName' (e.g. 'Project' or 'Operation')
+#>
+function Get-AzDevOpsApiUriAreaName
+{
+    [CmdletBinding()]
+    [OutputType([System.Object[]])]
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        [ValidateScript({ Test-AzDevOpsApiResourceName -ResourceName $_ -IsValid })]
+        [System.String]
+        $ResourceName
+    )
+
+    [hashtable]$resourceNameToApiUriAreaName = @{
+
+        Operation = 'core' # Note: Not explicitly listed here in documentation
+        Project = 'core'
+    }
+
+    if (![string]::IsNullOrWhiteSpace($ResourceName))
+    {
+        return $resourceNameToApiUriAreaName[$ResourceName]
+    }
+
+    return $resourceNameToApiUriAreaName.Values
+}
