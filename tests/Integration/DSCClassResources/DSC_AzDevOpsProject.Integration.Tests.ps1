a2ac5e12767a5d8371ca32a95350d4ae4a3a0200
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-03 20:51:42 +0000

    Setup initial, working. Integration tests for 'DSC_AzDevOpsProject', class-based, DSC resource.

---
+++ tests/Integration/DSCClassResources/DSC_AzDevOpsProject.Integration.Tests.ps1
@@ -0,0 +1,106 @@
+#Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1')
+
+if (-not (Test-BuildCategory -Type 'Integration'))
+{
+    return
+}
+
+$script:dscModuleName = 'AzureDevOpsDsc'
+$script:dscResourceFriendlyName = 'DSC_AzDevOpsProject'
+$script:dscResourceName = $script:dscResourceFriendlyName
+
+try
+{
+    Import-Module -Name DscResource.Test -Force -ErrorAction 'Stop'
+}
+catch [System.IO.FileNotFoundException]
+{
+    throw 'DscResource.Test module dependency not found. Please run ".\build.ps1 -Tasks build" first.'
+}
+
+$script:testEnvironment = Initialize-TestEnvironment `
+    -DSCModuleName $script:dscModuleName `
+    -DSCResourceName $script:dscResourceName `
+    -ResourceType 'Mof' `
+    -TestType 'Integration'
+
+
+try
+{
+    #$configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+    #. $configFile
+
+    Describe "$($script:dscResourceName)_Integration" {
+
+        BeforeAll {
+            $resourceId = "[$($script:dscResourceFriendlyName)]Integration_Test"
+            $configFile = Join-Path -Path $PSScriptRoot -ChildPath "$($script:dscResourceName).config.ps1"
+            . $configFile
+
+            $configurationName = "$($script:dscResourceName)_EnsureProjectPresent_Config"
+            $TestDrive = 'C:\TestDSC'
+        }
+
+
+        Context ('When using configuration {0}' -f $configurationName) {
+
+            It "Should exist - '$configFile'" {
+                Test-Path $configFile | Should -BeTrue
+            }
+
+            It "Should not be null or empty - '$configurationName'" {
+                $configurationName | Should -Not -BeNullOrEmpty
+            }
+
+
+            It 'Should compile and apply the MOF without throwing' {
+                {
+                    $configurationParameters = @{
+                        OutputPath           = $TestDrive
+                        # The variable $ConfigurationData was dot-sourced above.
+                        ConfigurationData    = $ConfigurationData
+                    }
+
+                    . $configFile
+                    & $configurationName @configurationParameters
+
+
+                    $startDscConfigurationParameters = @{
+                        Path         = $TestDrive
+                        ComputerName = 'localhost'
+                        Wait         = $true
+                        Verbose      = $true
+                        Force        = $true
+                        ErrorAction  = 'Stop'
+                    }
+
+                    Start-DscConfiguration @startDscConfigurationParameters
+                } | Should -Not -Throw
+            }
+
+            It 'Should be able to call Get-DscConfiguration without throwing' {
+                {
+                    $script:currentConfiguration = Get-DscConfiguration -Verbose -ErrorAction Stop
+                } | Should -Not -Throw
+            }
+
+            It 'Should have set the resource and all the parameters should match' {
+                $resourceCurrentState = $script:currentConfiguration | Where-Object -FilterScript {
+                    $_.ConfigurationName -eq $configurationName `
+                    -and $_.ResourceId -eq $resourceId
+                }
+
+                $resourceCurrentState.ProjectName | Should -Be 'TestProjectName'
+                $resourceCurrentState.ProjectDescription | Should -Be 'TestProjectDescription'
+            }
+
+            It 'Should return $true when Test-DscConfiguration is run' {
+                Test-DscConfiguration -Verbose | Should -Be 'True'
+            }
+        }
+    }
+}
+finally
+{
+    Restore-TestEnvironment -TestEnvironment $script:testEnvironment
+}
