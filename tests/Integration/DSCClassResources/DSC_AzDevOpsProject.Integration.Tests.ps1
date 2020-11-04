9380720ac786d31af86c2f89b033c14242596019
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-04 16:24:11 +0000

    Removed non-required, suib-'Context' scopes from 'DSC_AzDevOpsProject' integration tests.

--- tests/Integration/DSCClassResources/DSC_AzDevOpsProject.Integration.Tests.ps1
+++ tests/Integration/DSCClassResources/DSC_AzDevOpsProject.Integration.Tests.ps1
@@ -41,61 +41,58 @@ try
 
         Context ("When compiling, applying and testing the MOF - '$($script:dscResourceName)_EnsureGitProjectPresent_Config'") {
 
-
-            Context ("When compiling, applying and testing the MOF") {
-
-                BeforeAll {
-                    $configurationName = "$($script:dscResourceName)_EnsureGitProjectPresent_Config"
-                    $resourceId = "[$($script:dscResourceFriendlyName)]Integration_Test_EnsureGitProjectPresent"
-                }
-
-                It 'Should not throw when compiling MOF and when calling "Start-DscConfiguration"' {
-                    {
-                        $configurationParameters = @{
-                            OutputPath           = $TestDrive
-                            # The variable $ConfigurationData was dot-sourced above.
-                            ConfigurationData    = $ConfigurationData
-                        }
-
-                        . $configFile
-                        & $configurationName @configurationParameters
-
-                        $startDscConfigurationParameters = @{
-                            Path         = $TestDrive
-                            ComputerName = 'localhost'
-                            Wait         = $true
-                            Verbose      = $true
-                            Force        = $true
-                            ErrorAction  = 'Stop'
-                        }
-
-                        Start-DscConfiguration @startDscConfigurationParameters
-                    } | Should -Not -Throw
-                }
+            BeforeAll {
+                $configurationName = "$($script:dscResourceName)_EnsureGitProjectPresent_Config"
+                $resourceId = "[$($script:dscResourceFriendlyName)]Integration_Test_EnsureGitProjectPresent"
+            }
 
 
-                It 'Should not throw when calling "Get-DscConfiguration"' {
-                    {
-                        $script:currentConfiguration = Get-DscConfiguration -Verbose -ErrorAction Stop
-                    } | Should -Not -Throw
-                }
-
-
-                It 'Should have set the resource and all the parameters should match' {
-                    $resourceCurrentState = $script:currentConfiguration | Where-Object -FilterScript {
-                        $_.ConfigurationName -eq $configurationName `
-                        -and $_.ResourceId -eq $resourceId
+            It 'Should not throw when compiling MOF and when calling "Start-DscConfiguration"' {
+                {
+                    $configurationParameters = @{
+                        OutputPath           = $TestDrive
+                        # The variable $ConfigurationData was dot-sourced above.
+                        ConfigurationData    = $ConfigurationData
                     }
 
-                    $resourceCurrentState.ProjectName | Should -Be 'TestGitProjectName'
-                    $resourceCurrentState.ProjectDescription | Should -Be 'TestGitProjectDescription'
-                    $resourceCurrentState.SourceControlType | Should -Be 'Git'
-                }
+                    . $configFile
+                    & $configurationName @configurationParameters
+
+                    $startDscConfigurationParameters = @{
+                        Path         = $TestDrive
+                        ComputerName = 'localhost'
+                        Wait         = $true
+                        Verbose      = $true
+                        Force        = $true
+                        ErrorAction  = 'Stop'
+                    }
+
+                    Start-DscConfiguration @startDscConfigurationParameters
+                } | Should -Not -Throw
+            }
 
 
-                It 'Should return $true when Test-DscConfiguration is run' {
-                    Test-DscConfiguration -Verbose | Should -Be 'True'
+            It 'Should not throw when calling "Get-DscConfiguration"' {
+                {
+                    $script:currentConfiguration = Get-DscConfiguration -Verbose -ErrorAction Stop
+                } | Should -Not -Throw
+            }
+
+
+            It 'Should have set the resource and all the parameters should match' {
+                $resourceCurrentState = $script:currentConfiguration | Where-Object -FilterScript {
+                    $_.ConfigurationName -eq $configurationName `
+                    -and $_.ResourceId -eq $resourceId
                 }
+
+                $resourceCurrentState.ProjectName | Should -Be 'TestGitProjectName'
+                $resourceCurrentState.ProjectDescription | Should -Be 'TestGitProjectDescription'
+                $resourceCurrentState.SourceControlType | Should -Be 'Git'
+            }
+
+
+            It 'Should return $true when Test-DscConfiguration is run' {
+                Test-DscConfiguration -Verbose | Should -Be 'True'
             }
         }
 
@@ -103,61 +100,58 @@ try
 
         Context ("When compiling, applying and testing the MOF - '$($script:dscResourceName)_EnsureTfvcProjectPresent_Config'") {
 
-
-            Context ("When compiling, applying and testing the MOF") {
-
-                BeforeAll {
-                    $configurationName = "$($script:dscResourceName)_EnsureTfvcProjectPresent_Config"
-                    $resourceId = "[$($script:dscResourceFriendlyName)]Integration_Test_EnsureTfvcProjectPresent"
-                }
-
-                It 'Should not throw when compiling MOF and when calling "Start-DscConfiguration"' {
-                    {
-                        $configurationParameters = @{
-                            OutputPath           = $TestDrive
-                            # The variable $ConfigurationData was dot-sourced above.
-                            ConfigurationData    = $ConfigurationData
-                        }
-
-                        . $configFile
-                        & $configurationName @configurationParameters
-
-                        $startDscConfigurationParameters = @{
-                            Path         = $TestDrive
-                            ComputerName = 'localhost'
-                            Wait         = $true
-                            Verbose      = $true
-                            Force        = $true
-                            ErrorAction  = 'Stop'
-                        }
-
-                        Start-DscConfiguration @startDscConfigurationParameters
-                    } | Should -Not -Throw
-                }
+            BeforeAll {
+                $configurationName = "$($script:dscResourceName)_EnsureTfvcProjectPresent_Config"
+                $resourceId = "[$($script:dscResourceFriendlyName)]Integration_Test_EnsureTfvcProjectPresent"
+            }
 
 
-                It 'Should not throw when calling "Get-DscConfiguration"' {
-                    {
-                        $script:currentConfiguration = Get-DscConfiguration -Verbose -ErrorAction Stop
-                    } | Should -Not -Throw
-                }
-
-
-                It 'Should have set the resource and all the parameters should match' {
-                    $resourceCurrentState = $script:currentConfiguration | Where-Object -FilterScript {
-                        $_.ConfigurationName -eq $configurationName `
-                        -and $_.ResourceId -eq $resourceId
+            It 'Should not throw when compiling MOF and when calling "Start-DscConfiguration"' {
+                {
+                    $configurationParameters = @{
+                        OutputPath           = $TestDrive
+                        # The variable $ConfigurationData was dot-sourced above.
+                        ConfigurationData    = $ConfigurationData
                     }
 
-                    $resourceCurrentState.ProjectName | Should -Be 'TestTfvcProjectName'
-                    $resourceCurrentState.ProjectDescription | Should -Be 'TestTfvcProjectDescription'
-                    $resourceCurrentState.SourceControlType | Should -Be 'Tfvc'
-                }
+                    . $configFile
+                    & $configurationName @configurationParameters
+
+                    $startDscConfigurationParameters = @{
+                        Path         = $TestDrive
+                        ComputerName = 'localhost'
+                        Wait         = $true
+                        Verbose      = $true
+                        Force        = $true
+                        ErrorAction  = 'Stop'
+                    }
+
+                    Start-DscConfiguration @startDscConfigurationParameters
+                } | Should -Not -Throw
+            }
 
 
-                It 'Should return $true when Test-DscConfiguration is run' {
-                    Test-DscConfiguration -Verbose | Should -Be 'True'
+            It 'Should not throw when calling "Get-DscConfiguration"' {
+                {
+                    $script:currentConfiguration = Get-DscConfiguration -Verbose -ErrorAction Stop
+                } | Should -Not -Throw
+            }
+
+
+            It 'Should have set the resource and all the parameters should match' {
+                $resourceCurrentState = $script:currentConfiguration | Where-Object -FilterScript {
+                    $_.ConfigurationName -eq $configurationName `
+                    -and $_.ResourceId -eq $resourceId
                 }
+
+                $resourceCurrentState.ProjectName | Should -Be 'TestTfvcProjectName'
+                $resourceCurrentState.ProjectDescription | Should -Be 'TestTfvcProjectDescription'
+                $resourceCurrentState.SourceControlType | Should -Be 'Tfvc'
+            }
+
+
+            It 'Should return $true when Test-DscConfiguration is run' {
+                Test-DscConfiguration -Verbose | Should -Be 'True'
             }
         }
 
@@ -165,124 +159,119 @@ try
 
         Context ("When compiling, applying and testing the MOF - '$($script:dscResourceName)_EnsureProjectPresent_Config'") {
 
-
-            Context ("When compiling, applying and testing the MOF") {
-
-                BeforeAll {
-                    $configurationName = "$($script:dscResourceName)_EnsureProjectPresent_Config"
-                    $resourceId = "[$($script:dscResourceFriendlyName)]Integration_Test_EnsureProjectPresent"
-                }
-
-                It 'Should not throw when compiling MOF and when calling "Start-DscConfiguration"' {
-                    {
-                        $configurationParameters = @{
-                            OutputPath           = $TestDrive
-                            # The variable $ConfigurationData was dot-sourced above.
-                            ConfigurationData    = $ConfigurationData
-                        }
-
-                        . $configFile
-                        & $configurationName @configurationParameters
-
-                        $startDscConfigurationParameters = @{
-                            Path         = $TestDrive
-                            ComputerName = 'localhost'
-                            Wait         = $true
-                            Verbose      = $true
-                            Force        = $true
-                            ErrorAction  = 'Stop'
-                        }
-
-                        Start-DscConfiguration @startDscConfigurationParameters
-                    } | Should -Not -Throw
-                }
+            BeforeAll {
+                $configurationName = "$($script:dscResourceName)_EnsureProjectPresent_Config"
+                $resourceId = "[$($script:dscResourceFriendlyName)]Integration_Test_EnsureProjectPresent"
+            }
 
 
-                It 'Should not throw when calling "Get-DscConfiguration"' {
-                    {
-                        $script:currentConfiguration = Get-DscConfiguration -Verbose -ErrorAction Stop
-                    } | Should -Not -Throw
-                }
-
-
-                It 'Should have set the resource and all the parameters should match' {
-                    $resourceCurrentState = $script:currentConfiguration | Where-Object -FilterScript {
-                        $_.ConfigurationName -eq $configurationName `
-                        -and $_.ResourceId -eq $resourceId
+            It 'Should not throw when compiling MOF and when calling "Start-DscConfiguration"' {
+                {
+                    $configurationParameters = @{
+                        OutputPath           = $TestDrive
+                        # The variable $ConfigurationData was dot-sourced above.
+                        ConfigurationData    = $ConfigurationData
                     }
 
-                    # These are all defaults from values provided in configuration data
-                    $resourceCurrentState.ProjectName | Should -Be 'TestProjectName'
-                    $resourceCurrentState.ProjectDescription | Should -Be 'TestProjectDescription'
-                    $resourceCurrentState.SourceControlType | Should -Be 'Git'
-                }
+                    . $configFile
+                    & $configurationName @configurationParameters
 
+                    $startDscConfigurationParameters = @{
+                        Path         = $TestDrive
+                        ComputerName = 'localhost'
+                        Wait         = $true
+                        Verbose      = $true
+                        Force        = $true
+                        ErrorAction  = 'Stop'
+                    }
 
-                It 'Should return $true when Test-DscConfiguration is run' {
-                    Test-DscConfiguration -Verbose | Should -Be 'True'
-                }
+                    Start-DscConfiguration @startDscConfigurationParameters
+                } | Should -Not -Throw
             }
+
+
+            It 'Should not throw when calling "Get-DscConfiguration"' {
+                {
+                    $script:currentConfiguration = Get-DscConfiguration -Verbose -ErrorAction Stop
+                } | Should -Not -Throw
+            }
+
+
+            It 'Should have set the resource and all the parameters should match' {
+                $resourceCurrentState = $script:currentConfiguration | Where-Object -FilterScript {
+                    $_.ConfigurationName -eq $configurationName `
+                    -and $_.ResourceId -eq $resourceId
+                }
+
+                # These are all defaults from values provided in configuration data
+                $resourceCurrentState.ProjectName | Should -Be 'TestProjectName'
+                $resourceCurrentState.ProjectDescription | Should -Be 'TestProjectDescription'
+                $resourceCurrentState.SourceControlType | Should -Be 'Git'
+            }
+
+
+            It 'Should return $true when Test-DscConfiguration is run' {
+                Test-DscConfiguration -Verbose | Should -Be 'True'
+            }
+
         }
 
 
 
         Context ("When compiling, applying and testing the MOF - '$($script:dscResourceName)_EnsureProjectUpdated_Config'") {
 
-            Context ("When compiling, applying and testing the MOF") {
-
-                BeforeAll {
-                    $configurationName = "$($script:dscResourceName)_EnsureProjectUpdated_Config"
-                    $resourceId = "[$($script:dscResourceFriendlyName)]Integration_Test_EnsureProjectUpdated"
-                }
+            BeforeAll {
+                $configurationName = "$($script:dscResourceName)_EnsureProjectUpdated_Config"
+                $resourceId = "[$($script:dscResourceFriendlyName)]Integration_Test_EnsureProjectUpdated"
+            }
 
 
-                It 'Should not throw when compiling MOF and when calling "Start-DscConfiguration"' {
-                    {
-                        $configurationParameters = @{
-                            OutputPath           = $TestDrive
-                            # The variable $ConfigurationData was dot-sourced above.
-                            ConfigurationData    = $ConfigurationData
-                        }
-
-                        . $configFile
-                        & $configurationName @configurationParameters
-
-                        $startDscConfigurationParameters = @{
-                            Path         = $TestDrive
-                            ComputerName = 'localhost'
-                            Wait         = $true
-                            Verbose      = $true
-                            Force        = $true
-                            ErrorAction  = 'Stop'
-                        }
-
-                        Start-DscConfiguration @startDscConfigurationParameters
-                    } | Should -Not -Throw
-                }
-
-
-                It 'Should not throw when calling "Get-DscConfiguration"' {
-                    {
-                        $script:currentConfiguration = Get-DscConfiguration -Verbose -ErrorAction Stop
-                    } | Should -Not -Throw
-                }
-
-
-                It 'Should have set the resource and all the parameters should match' {
-                    $resourceCurrentState = $script:currentConfiguration | Where-Object -FilterScript {
-                        $_.ConfigurationName -eq $configurationName `
-                        -and $_.ResourceId -eq $resourceId
+            It 'Should not throw when compiling MOF and when calling "Start-DscConfiguration"' {
+                {
+                    $configurationParameters = @{
+                        OutputPath           = $TestDrive
+                        # The variable $ConfigurationData was dot-sourced above.
+                        ConfigurationData    = $ConfigurationData
                     }
 
-                    $resourceCurrentState.ProjectName | Should -Be 'TestProjectName'
-                    $resourceCurrentState.ProjectDescription | Should -Be 'AnAmendedProjectDescription'
-                    $resourceCurrentState.SourceControlType | Should -Be 'Git' # Must be the same (change not supported with this)
-                }
+                    . $configFile
+                    & $configurationName @configurationParameters
+
+                    $startDscConfigurationParameters = @{
+                        Path         = $TestDrive
+                        ComputerName = 'localhost'
+                        Wait         = $true
+                        Verbose      = $true
+                        Force        = $true
+                        ErrorAction  = 'Stop'
+                    }
+
+                    Start-DscConfiguration @startDscConfigurationParameters
+                } | Should -Not -Throw
+            }
 
 
-                It 'Should return $true when Test-DscConfiguration is run' {
-                    Test-DscConfiguration -Verbose | Should -Be 'True'
+            It 'Should not throw when calling "Get-DscConfiguration"' {
+                {
+                    $script:currentConfiguration = Get-DscConfiguration -Verbose -ErrorAction Stop
+                } | Should -Not -Throw
+            }
+
+
+            It 'Should have set the resource and all the parameters should match' {
+                $resourceCurrentState = $script:currentConfiguration | Where-Object -FilterScript {
+                    $_.ConfigurationName -eq $configurationName `
+                    -and $_.ResourceId -eq $resourceId
                 }
+
+                $resourceCurrentState.ProjectName | Should -Be 'TestProjectName'
+                $resourceCurrentState.ProjectDescription | Should -Be 'AnAmendedProjectDescription'
+                $resourceCurrentState.SourceControlType | Should -Be 'Git' # Must be the same (change not supported with this)
+            }
+
+
+            It 'Should return $true when Test-DscConfiguration is run' {
+                Test-DscConfiguration -Verbose | Should -Be 'True'
             }
         }
 
