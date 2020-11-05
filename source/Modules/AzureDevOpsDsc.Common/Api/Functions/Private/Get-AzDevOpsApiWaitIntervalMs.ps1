e5825218ebbcfe78ffa904d9169729cc41390ee1
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-05 09:45:39 +0000

    Added 'Get-AzDevOpsApiWaitIntervalMs' function

---
+++ source/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Get-AzDevOpsApiWaitIntervalMs.ps1
@@ -0,0 +1,19 @@
+<#
+    .SYNOPSIS
+        Returns the number of Milliseconds that will be waited for between calls
+        to the Azure DevOps API.
+
+    .EXAMPLE
+        Get-AzDevOpsApiWaitIntervalMs
+
+        Returns the number of Milliseconds that will be waited for between calls
+        to the Azure DevOps API.
+#>
+function Get-AzDevOpsApiWaitIntervalMs
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    param ()
+
+    return 250
+}
