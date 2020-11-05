b1304f9485e772e73ee6730ae9d7dcf6c2a2a522
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-05 09:45:48 +0000

    Added 'Get-AzDevOpsApiWaitTimeoutMs'

---
+++ source/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Get-AzDevOpsApiWaitTimeoutMs.ps1
@@ -0,0 +1,19 @@
+<#
+    .SYNOPSIS
+        Returns the number of Milliseconds that will be waited for calls to
+        the Azure DevOps API to timeout (during a 'Wait' operation).
+
+    .EXAMPLE
+        Get-AzDevOpsApiWaitTimeoutMs
+
+        Returns the number of Milliseconds that will be waited for calls to
+        the Azure DevOps API to timeout (during a 'Wait' operation).
+#>
+function Get-AzDevOpsApiWaitTimeoutMs
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    param ()
+
+    return 10000
+}
