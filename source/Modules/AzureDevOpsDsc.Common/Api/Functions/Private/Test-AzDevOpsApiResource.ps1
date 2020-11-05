d6c28ad84c00b0594835053b7a21af340a15a132
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-05 09:22:21 +0000

    Added 'Test-AzDevOpsApiResource' function

---
+++ source/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Test-AzDevOpsApiResource.ps1
@@ -0,0 +1,65 @@
+<#
+    .SYNOPSIS
+        Tests for the presence of an Azure DevOps API Resource.
+
+    .PARAMETER ApiUri
+        The URI of the Azure DevOps API to be connected to. For example:
+
+          https://dev.azure.com/someOrganizationName/_apis/
+
+    .PARAMETER Pat
+        The 'Personal Access Token' (PAT) to be used by any subsequent requests/Resources
+        against the Azure DevOps API. This PAT must have the relevant permissions assigned
+        for the subsequent Resources being performed.
+
+    .PARAMETER ResourceName
+        The name of the resource being updated within Azure DevOps (e.g. 'Project')
+
+    .PARAMETER ResourceId
+        The 'id' of the Azure DevOps API Resource. This is typically obtained from a response
+        provided by the API when a request is made to it.
+
+    .EXAMPLE
+        Test-AzDevOpsApiResource -ApiUri 'YourApiUriHere' -Pat 'YourPatHere' `
+                                 -ResourceName 'YourResourceName' -ResourceId 'YourResourceId'
+
+        Tests that the Azure DevOps 'Resource' (identified by the 'ResourceId' for the resource of type
+        provided by the 'ResourceName' field) exists. Returns $true if it exists and returns $false
+        if it does not.
+#>
+function Test-AzDevOpsApiResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [ValidateScript( { Test-AzDevOpsApiUri -ApiUri $_ -IsValid })]
+        [Alias('Uri')]
+        [System.String]
+        $ApiUri,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-AzDevOpsPat -Pat $_ -IsValid })]
+        [Alias('PersonalAccessToken')]
+        [System.String]
+        $Pat,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-AzDevOpsApiResourceName -ResourceName $_ -IsValid })]
+        [System.String]
+        $ResourceName,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateScript({ Test-AzDevOpsResourceId -ResourceId $_ -IsValid })]
+        [Alias('Id')]
+        [System.String]
+        $ResourceId
+    )
+
+    [object[]]$apiResource = Get-AzDevOpsApiResource -ApiUri $ApiUri -Pat $Pat `
+                                                     -ResourceName $ResourceName `
+                                                     -ResourceId $ResourceId
+
+    return ($apiResource.Count -gt 0)
+}
