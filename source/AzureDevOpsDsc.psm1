333311585c16dc9dda4281d56692b53ba540765c
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-05 14:31:44 +0000

    Added a number of hidden properties and functions into 'DSC_AzDevOpsResource' class.

--- source/AzureDevOpsDsc.psm1
+++ source/AzureDevOpsDsc.psm1
@@ -26,12 +26,9 @@ enum RequiredFunction
 }
 
 
+
 class DSC_AzDevOpsResource
 {
-    [DscProperty()]
-    [Ensure]$Ensure
-
-
     [DscProperty()]
     [Alias('Uri')]
     [string]$ApiUri
@@ -39,6 +36,235 @@ class DSC_AzDevOpsResource
     [DscProperty()]
     [Alias('PersonalAccessToken')]
     [string]$Pat
+
+    [DscProperty()]
+    [Ensure]$Ensure
+
+
+    # Non-DSC properties for use in operations/comparisons
+
+
+    # Hidden properties
+    hidden [string]$ResourceKey
+    hidden [string]$ResourceKeyPropertyName
+    hidden [string]$ResourceName = $this.GetResourceName()
+    hidden [hashtable]$ResourceProperties = $this.GetResourceProperties()
+
+
+    # Constructor(s)
+    DSC_AzDevOpsResource(){}
+
+
+    # DSC-specific methods
+
+    hidden [string[]]GetDscResourceDscPropertyNames()
+    {
+        [string[]]$thisDscPropertyNames = @()
+
+        [Type]$thisType = $this.GetType()
+
+        $thisProperties = $thisType.GetProperties()
+        $thisProperties | ForEach-Object {
+
+            $PropertyName = $_.Name
+
+            $_.GetCustomAttributes($true) |
+            ForEach-Object {
+
+                if ($_.TypeId.Name -eq 'DscPropertyAttribute')
+                {
+                    $thisDscPropertyNames += $PropertyName
+                }
+            }
+        }
+
+        return $thisDscPropertyNames
+    }
+
+    hidden [string]GetDscResourceDscKeyPropertyName()
+    {
+        [string[]]$thisDscKeyPropertyNames = @()
+
+        $thisProperties = $this.GetDscResourceDscPropertyNames()
+        $thisProperties | ForEach-Object {
+
+            $PropertyName = $_.Name
+
+            $_.GetCustomAttributes($true) |
+            ForEach-Object {
+
+                if ($_.TypeId.Name -eq 'DscPropertyAttribute' -and
+                    $_.Key -eq $true)
+                {
+                    $thisDscKeyPropertyNames += $PropertyName
+                }
+            }
+        }
+
+        if ($null -eq $thisDscKeyPropertyNames -or $thisDscKeyPropertyNames.Count -eq 0)
+        {
+            throw "Could not obtain a 'DscResourceDscKey' property for type '$($this.GetType().Name)'."
+        }
+        elseif ($thisDscKeyPropertyNames.Count -gt 1)
+        {
+            throw "Obtained more than 1 property for type '$($this.GetType().Name)' that was marked as a 'Key'. There must only be 1 property on the class set as the 'Key' for DSC."
+        }
+
+        return $thisDscKeyPropertyNames[0]
+    }
+
+
+
+
+    # Non DSC-specific methods
+
+    hidden [string]GetResourceName()
+    {
+        # Assumes a naming convention is followed between the DSC
+        # resource name and the name of the resource within the API
+        return $this.GetType().ToString().Replace('DSC_AzDevOps','')
+    }
+
+
+    hidden [string]GetResourceKeyPropertyName()
+    {
+        # Uses the same value as the 'DscResourceDscKeyPropertyName'
+        return $this.GetDscResourceDscKeyPropertyName()
+    }
+
+
+    hidden [string]GetResourceKey()
+    {
+        [string]$keyPropertyName = $this.GetResourceKeyPropertyName()
+
+        if ([string]::IsNullOrWhiteSpace($keyPropertyName))
+        {
+            return $null
+        }
+
+        return $this."$keyPropertyName"
+    }
+
+
+    # This method must be overidden by inheriting classes
+    hidden [string]GetResourceAlternateKeyPropertyName()
+    {
+        $thisType = $this.GetType()
+        if ($thisType -eq [DSC_AzDevOpsResource])
+        {
+            throw "Method 'GetResourceAlternateKeyPropertyName()' in '$($thisType.Name)' must be overidden by an inheriting class."
+            return $null
+        }
+        return $null
+    }
+
+
+    hidden [string]GetResourceAlternateKey()
+    {
+        [string]$alternateKeyPropertyName = $this.GetResourceAlternateKeyPropertyName()
+
+        if ([string]::IsNullOrWhiteSpace($alternateKeyPropertyName))
+        {
+            return $null
+        }
+
+        return $this."$alternateKeyPropertyName"
+    }
+
+
+    hidden [Hashtable]GetResourceProperties()
+    {
+        [PSObject]$thisObject = $this
+        [Hashtable]$thisProperties = @{}
+
+        $thisObject.PSObject.Properties | ForEach-Object {
+            $thisProperties[$_.Name] = $_.Value
+        }
+
+        return $thisProperties
+    }
+
+
+    hidden [string]GetResourceGetMethodName()
+    {
+        $thisResourceName = $this.GetResourceName()
+        return "Get-AzDevOps$thisResourceName"
+    }
+    hidden [string]GetResourceSetMethodName()
+    {
+        $thisResourceName = $this.GetResourceName()
+        return "Set-AzDevOps$thisResourceName"
+    }
+    hidden [string]GetResourceRemoveMethodName()
+    {
+        $thisResourceName = $this.GetResourceName()
+        return "Remove-AzDevOps$thisResourceName"
+    }
+    hidden [string]GetResourceTestMethodName()
+    {
+        $thisResourceName = $this.GetResourceName()
+        return "Test-AzDevOps$thisResourceName"
+    }
+
+
+    hidden [object]GetCurrentStateResourceObject()
+    {
+        [string]$thisResourceKey = $this.GetResourceKey()
+        [string]$thisResourceAlternateKey = $this.GetResourceAlternateKey()
+        [string]$thisResourceGetMethodName = $this.GetResourceGetMethodName()
+
+        $getParameters = @{
+            ApiUri             = $this.ApiUri
+            Pat                = $this.Pat
+            "$thisResourceKey" = $this."$thisResourceKey"
+        }
+
+        if (![string]::IsNullOrWhiteSpace($this."$thisResourceAlternateKey"))
+        {
+            $getParameters."$thisResourceAlternateKey" = $this."$thisResourceAlternateKey"
+        }
+
+        return & $thisResourceGetMethodName @getParameters
+    }
+
+
+    [Hashtable]GetCurrentStateProperties()
+    {
+        # Obtain 'CurrentStateResourceObject' and pass into overidden function of inheriting class
+        return $this.GetCurrentStateProperties($this.GetCurrentStateResourceObject())
+    }
+
+    # This method must be overidden by inheriting classes
+    [Hashtable]GetCurrentStateProperties([object]$ResourceObject)
+    {
+        $thisType = $this.GetType()
+        if ($thisType -eq [DSC_AzDevOpsResource])
+        {
+            throw "Method 'GetCurrentState()' in '$($thisType.Name)' must be overidden by an inheriting class."
+            return $null
+        }
+        return $null
+    }
+
+    [Hashtable]GetDesiredStateProperties()
+    {
+        return $this.GetResourceProperties()
+    }
+
+
+    [bool]IsInDesiredState(
+        [hashtable]$Desired,
+        [hashtable]$Current)
+    {
+        $thisType = $this.GetType()
+        if ($thisType -eq [DSC_AzDevOpsResource])
+        {
+            throw "Method 'IsInDesiredState()' in '$($thisType.Name)' must be overidden by an inheriting class."
+            return $null
+        }
+        return $null
+    }
+
 }
 
 
