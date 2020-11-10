b24cbd2785279e0fb49823def9429f3592ed7800
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-10 10:17:17 +0000

    Renamed 'Test-AzDevOpsCredential' to 'Test-AzDevOpsPatCredential' and updated validation rules.

--- source/Modules/AzureDevOpsDsc.Common/Connection/Functions/Private/Test-AzDevOpsPatCredential.ps1
+++ source/Modules/AzureDevOpsDsc.Common/Connection/Functions/Private/Test-AzDevOpsPatCredential.ps1
@@ -1,26 +1,26 @@
 <#
     .SYNOPSIS
-        Peforms test on a provided 'Credential' to provide a boolean ($true or $false)
+        Peforms test on a provided '(Pat)Credential' to provide a boolean ($true or $false)
         return value. Returns $true if the test is successful.
 
         NOTE: Use of the '-IsValid' switch is required.
 
-    .PARAMETER Credential
-        The 'Credential' to be tested/validated.
+    .PARAMETER PatCredential
+        The '(Pat)Credential' containing the Personal Access Token (PAT) to be tested/validated.
 
     .PARAMETER IsValid
-        Use of this switch will validate the format of the 'Credential'
+        Use of this switch will validate the format of the '(Pat)Credential'
         rather than the existence/presence of it.
 
         Failure to use this switch will throw an exception.
 
     .EXAMPLE
-        Test-AzDevOpsCredential -Credential 'YourCredentialHere' -IsValid
+        Test-AzDevOpsCredential -PatCredential $YourPatCredentialHere$ -IsValid
 
-        Returns $true if the 'Credential' provided is of a valid format.
+        Returns $true if the '(Pat)Credential' provided is of a valid format.
         Returns $false if it is not.
 #>
-function Test-AzDevOpsCredential
+function Test-AzDevOpsPatCredential
 {
     [CmdletBinding()]
     [OutputType([System.Boolean])]
@@ -28,7 +28,8 @@ function Test-AzDevOpsCredential
     (
         [Parameter(Mandatory = $true)]
         [System.Management.Automation.PSCredential]
-        $Credential,
+        [Alias('Credential')]
+        $PatCredential,
 
         [Parameter(Mandatory = $true)]
         [System.Management.Automation.SwitchParameter]
@@ -41,8 +42,11 @@ function Test-AzDevOpsCredential
         New-InvalidOperationException -Message $errorMessage
     }
 
-    # TODO:
-    #  - Validate username is 'PAT' ?
+    if ($null -eq $PatCredential -or
+        'PAT' -ne $PatCredential.UserName)
+    {
+            return $false
+    }
 
     return $true
 }
