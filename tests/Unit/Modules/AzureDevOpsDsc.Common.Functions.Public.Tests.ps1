d82a12529e6813c4edb72af47ac757c0e7ceb8d5
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-10 08:02:28 +0000

    Added functionality for testing combinations of ParameterSetTestCases with all combinations of 'Valid' parameter values for each parameter in parameter set.

--- tests/Unit/Modules/AzureDevOpsDsc.Common.Functions.Public.Tests.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc.Common.Functions.Public.Tests.ps1
@@ -18,10 +18,53 @@ InModuleScope $script:subModuleName {
             [hashtable]$testCase = $_
             $testCase.Add('CommandName',$CommandName)
             $testCase.Add('ParameterSetName',$ParameterSetName)
+            $testCase.Add('ParameterNames',$_.ParameterSetValues.Keys)
 
             $testCase
         }
     }
+
+    [hashtable[]]$testCasesValidCommandParameterSetNameValidParameterValues = $testCasesValidCommandParameterSetNames | Where-Object { $_.ParameterNames.Count -gt 0 } | ForEach-Object {
+
+            $commandName = $_.CommandName
+            $parameterNames = $_.ParameterNames
+
+            # Note: Exclude any parameter sets that do not have any parameters and ensure only
+            #       looping through the test cases for the 'CommandName' being looped through in outer loop
+            $testCasesValidCommandParameterSetNames | Where-Object { $_.ParameterNames.Count -gt 0 -and $_.CommandName -eq $commandName } | ForEach-Object {
+
+                    $testCase = $_
+
+                    $parameterNames | ForEach-Object {
+
+                        $parameterName = $_
+                        $parameterValues = $(Get-TestCaseValue -ScopeName $parameterName -TestCaseName 'Valid')
+
+                        $parameterValues | ForEach-Object {
+
+                            $parameterValue = $_
+
+                            $newTestCase = @{}
+                            $testCase.Keys | ForEach-Object {
+                                $newTestCase[$_] = $testCase[$_]
+                            }
+                            $newTestCase.Remove('ParameterSetValues')
+                            $newTestCase.Add('ParameterSetValues',@{})
+                            $testCase.ParameterSetValues.Keys | ForEach-Object {
+                                $newTestCase.ParameterSetValues[$_] = $testCase.ParameterSetValues[$_]
+                            }
+                            $newTestCase.ParameterSetValues[$parameterName] = $parameterValue
+                            $newTestCase.Add('ParameterValue',$parameterValue)
+                            $newTestCase.Add('ParameterName',$parameterName)
+
+                            $newTestCase
+
+                        }
+                    }
+            }
+    }
+
+
 
     [hashtable[]]$testCasesInvalidCommandParameterSetNames = $script:publicCommandNames | ForEach-Object {
 
@@ -33,6 +76,7 @@ InModuleScope $script:subModuleName {
             [hashtable]$testCase = $_
             $testCase.Add('CommandName',$CommandName)
             $testCase.Add('ParameterSetName',$ParameterSetName)
+            $testCase.Add('ParameterNames',$_.Keys)
 
             $testCase
         }
@@ -57,7 +101,14 @@ InModuleScope $script:subModuleName {
 
                     { & $CommandName @ParameterSetValues } | Should -Not -Throw
                 }
+
+                It "Should not throw - '<CommandName>' - '<ParameterSetValuesKey>' - <ParameterSetValuesOffset> ('<ParameterName>' = '<ParameterValue>')" -TestCases $testCasesValidCommandParameterSetNameValidParameterValues {
+                    param([string]$CommandName, [Hashtable]$ParameterSetValues)
+
+                    { & $CommandName @ParameterSetValues } | Should -Not -Throw
+                }
             }
+
 
             Context "When invoking function/command with 'Invalid', parameter set values" {
 
