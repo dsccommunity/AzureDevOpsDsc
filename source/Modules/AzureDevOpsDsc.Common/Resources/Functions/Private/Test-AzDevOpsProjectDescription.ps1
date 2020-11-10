46f757ac7e0eea40236c1073aeaa29933d3f2e12
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-10 14:41:01 +0000

    Added 'ValidateSet($true)' attribute Added 'ValidateSet($true)' attribute to '' function, 'IsValid' parameter to 'Test-AzDevOpsProjectDescription' function, 'IsValid' parameter

--- source/Modules/AzureDevOpsDsc.Common/Resources/Functions/Private/Test-AzDevOpsProjectDescription.ps1
+++ source/Modules/AzureDevOpsDsc.Common/Resources/Functions/Private/Test-AzDevOpsProjectDescription.ps1
@@ -32,6 +32,7 @@ function Test-AzDevOpsProjectDescription
         $ProjectDescription,
 
         [Parameter(Mandatory = $true)]
+        [ValidateSet($true)]
         [System.Management.Automation.SwitchParameter]
         $IsValid
     )
