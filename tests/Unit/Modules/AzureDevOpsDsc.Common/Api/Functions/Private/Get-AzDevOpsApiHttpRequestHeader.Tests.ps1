03a0c6faccad226a2593144c5d6b54b965f6a227
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-10 18:07:46 +0000

    Added 'Get-AzDevOpsApiHttpRequestHeader' function, unit tests.

--- tests/Unit/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Get-AzDevOpsApiHttpRequestHeader.Tests.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Get-AzDevOpsApiHttpRequestHeader.Tests.ps1
@@ -1,76 +1,77 @@
 
 # Initialize tests for module function
-. $PSScriptRoot\..\..\..\AzureDevOpsDsc.Common.TestInitialization.ps1
+. $PSScriptRoot\..\..\..\..\AzureDevOpsDsc.Common.Tests.Initialization.ps1
 
 
 InModuleScope $script:subModuleName {
-    Describe 'AzureDevOpsDsc.Common\Get-AzDevOpsApiHttpRequestHeader' -Tag 'GetAzDevOpsApiHttpRequestHeader' {
+    $script:subModuleName = 'AzureDevOpsDsc.Common'
+    $script:commandName = $(Get-Item $PSCommandPath).BaseName.Replace('.Tests','')
+    $script:tag = @($($script:commandName -replace '-'))
 
-        Context 'When called with valid parameters' {
-            BeforeAll {
+    Describe "$script:subModuleName\Api\Function\$script:commandName" -Tag $script:tag {
+
+        $testCasesValidPats = Get-TestCase -ScopeName 'Pat' -TestCaseName 'Valid'
+        $testCasesInvalidPats = Get-TestCase -ScopeName 'Pat' -TestCaseName 'Invalid'
+
+        Context 'When input parameters are valid' {
+
+            It 'Should not throw - "<Pat>"' -TestCases $testCasesValidPats {
+                param ([System.String]$Pat)
+
+                { Get-AzDevOpsApiHttpRequestHeader -Pat $Pat } | Should -Not -Throw
             }
 
-            Context 'When called with valid "Pat" parameter' {
+            It 'Should output a "Hashtable" - "<Pat>"' -TestCases $testCasesValidPats {
+                param ([System.String]$Pat)
 
-                $testCasesValidPats = Get-TestCase -ScopeName 'Pat' -TestCaseName 'Valid'
+                $httpRequestHeader = Get-AzDevOpsApiHttpRequestHeader -Pat $Pat
 
-                It 'Should not throw - "<Pat>"' -TestCases $testCasesValidPats {
-                    param ([string]$Pat)
-
-                    { Get-AzDevOpsApiHttpRequestHeader -Pat $Pat } | Should -Not -Throw
-                }
-
-                It 'Should return "hashtable" - "<Pat>"' -TestCases $testCasesValidPats {
-                    param ([string]$Pat)
-
-                    $result = Get-AzDevOpsApiHttpRequestHeader -Pat $Pat
-                    $result.GetType() | Should -Be $($([hashtable]::new()).GetType())
-                }
-
-                It 'Should return correct "Authorization" hashtable property value - "<Pat>"' -TestCases $testCasesValidPats {
-                    param ([string]$Pat)
-
-                    $result = Get-AzDevOpsApiHttpRequestHeader -Pat $Pat
-                    $result.Authorization | Should -Be $('Basic ' +
-                        [Convert]::ToBase64String(
-                        [Text.Encoding]::ASCII.GetBytes(":$Pat")))
-                }
-
+                $httpRequestHeader.GetType() | Should -Be $(@{}.GetType())
             }
 
+            It 'Should output a "Hashtable" with an "Authorization" key - "<Pat>"' -TestCases $testCasesValidPats {
+                param ([System.String]$Pat)
 
+                $httpRequestHeader = Get-AzDevOpsApiHttpRequestHeader -Pat $Pat
+
+                $httpRequestHeader.ContainsKey('Authorization') | Should -BeTrue
+            }
+
+            It 'Should output a "Hashtable" with an "Authorization" key that has a value beginning with "Basic " - "<Pat>"' -TestCases $testCasesValidPats {
+                param ([System.String]$Pat)
+
+                $httpRequestHeader = Get-AzDevOpsApiHttpRequestHeader -Pat $Pat
+
+                $httpRequestHeader.Authorization | Should -BeLike "Basic *"
+            }
+
+            It 'Should output a "Hashtable" with an "Authorization" key that has a value as expected - "<Pat>" ' -TestCases $testCasesValidPats {
+                param ([System.String]$Pat)
+
+                $Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$Pat"))
+                $httpRequestHeader = Get-AzDevOpsApiHttpRequestHeader -Pat $Pat
+
+                $httpRequestHeader.Authorization | Should -BeExactly $Authorization
+            }
+
+            It 'Should output a "Hashtable" that is successfully validated by "Test-AzDevOpsApiHttpRequestHeader"' -TestCases $testCasesValidPats {
+                param ([System.String]$Pat)
+
+                $httpRequestHeader = Get-AzDevOpsApiHttpRequestHeader -Pat $Pat
+
+                Test-AzDevOpsApiHttpRequestHeader -HttpRequestHeader $httpRequestHeader -IsValid | Should -BeTrue
+            }
 
         }
 
-        Context 'When called with invalid parameters' {
-            BeforeAll {
+        Context "When input parameters are invalid" {
+
+            It 'Should throw - "<Pat>"' -TestCases $testCasesInvalidPats {
+                param ([System.String]$Pat)
+
+                { Get-AzDevOpsApiHttpRequestHeader -Pat $Pat } | Should -Throw
             }
 
-            Context 'When called with invalid "Pat" parameter' {
-
-                $testCasesInvalidPats = Get-TestCase -ScopeName 'Pat' -TestCaseName 'Invalid'
-
-                It "Should throw - '<Pat>'" -TestCases $testCasesInvalidPats {
-                    param ([string]$Pat)
-
-                    { Get-AzDevOpsApiHttpRequestHeader -Pat $Pat } | Should -Throw
-
-                }
-            }
-
-            Context 'When called with empty "Pat" parameter' {
-
-                $testCasesEmptyPats = Get-TestCase -ScopeName 'Pat' -TestCaseName 'Empty'
-
-                It "Should throw - '<Pat>'" -TestCases $testCasesEmptyPats {
-                    param ([string]$Pat)
-
-                    { Get-AzDevOpsApiHttpRequestHeader -Pat $Pat } | Should -Throw
-
-                }
-            }
         }
-
     }
-
 }
