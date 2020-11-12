61e3817f551f7fc593d6e9082f94d77670909ad4
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-12 10:55:53 +0000

    Removed, non-relevant tests from 'Wait-AzDevOpsOperation' function, unit tests

--- tests/Unit/Modules/AzureDevOpsDsc.Common/Resources/Functions/Private/Wait-AzDevOpsOperation.Tests.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/Resources/Functions/Private/Wait-AzDevOpsOperation.Tests.ps1
@@ -562,97 +562,12 @@ InModuleScope 'AzureDevOpsDsc.Common' {
                     }
                 }
             }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            # EXISTING - TO REMOVE BELOW
-            Context 'When called with "OperationId" parameter value and the "IsValid" switch' {
-
-
-                Context 'When "OperationId" parameter value is a valid "OperationId"' {
-
-                    It 'Should not throw - "<OperationId>"' -TestCases $testCasesValidOperationIds {
-                        param ([System.String]$OperationId)
-
-                        { Test-AzDevOpsOperationId -OperationId $OperationId -IsValid } | Should -Not -Throw
-                    }
-
-                    It 'Should return $true - "<OperationId>"' -TestCases $testCasesValidOperationIds {
-                        param ([System.String]$OperationId)
-
-                        Test-AzDevOpsOperationId -OperationId $OperationId -IsValid | Should -BeTrue
-                    }
-                }
-
-
-                Context 'When "OperationId" parameter value is an invalid "OperationId"' {
-
-                    It 'Should not throw - "<OperationId>"' -TestCases $testCasesInvalidOperationIds {
-                        param ([System.String]$OperationId)
-
-                        { Test-AzDevOpsOperationId -OperationId $OperationId -IsValid } | Should -Not -Throw
-                    }
-
-                    It 'Should return $false - "<OperationId>"' -TestCases $testCasesInvalidOperationIds {
-                        param ([System.String]$OperationId)
-
-                        Test-AzDevOpsOperationId -OperationId $OperationId -IsValid | Should -BeFalse
-                    }
-                }
-            }
         }
 
 
         Context "When input parameters are invalid" {
 
-
-            Context 'When called with no/null parameter values/switches' {
-
-                It 'Should throw' {
-
-                    { Test-AzDevOpsOperationId -OperationId:$null -IsValid:$false } | Should -Throw
-                }
-            }
-
-
-            Context 'When "OperationId" parameter value is a valid "OperationId"' {
-
-
-                Context 'When called with "OperationId" parameter value but a $false "IsValid" switch value' {
-
-                    It 'Should throw - "<OperationId>"' -TestCases $testCasesValidOperationIds {
-                        param ([System.String]$OperationId)
-
-                        { Test-AzDevOpsOperationId -OperationId $OperationId -IsValid:$false } | Should -Throw
-                    }
-                }
-            }
-
-
-            Context 'When "OperationId" parameter value is an invalid "OperationId"' {
-
-
-                Context 'When called with "OperationId" parameter value but a $false "IsValid" switch value' {
-
-                    It 'Should throw - "<OperationId>"' -TestCases $testCasesInvalidOperationIds {
-                        param ([System.String]$OperationId)
-
-                        { Test-AzDevOpsOperationId -OperationId $OperationId -IsValid:$false } | Should -Throw
-                    }
-                }
-            }
-
+            # TODO
 
         }
     }
