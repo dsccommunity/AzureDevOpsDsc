ae22b94d07b0c9075b4ea42454508cbe24a13b93
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-13 15:18:22 +0000

    Added 'Get-AzDevOpsProject' function, unit tests for calls using both 'ProjectId' and 'ProjectDescription' parameters.

--- tests/Unit/Modules/AzureDevOpsDsc.Common/Resources/Functions/Public/Get-AzDevOpsProject.Tests.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/Resources/Functions/Public/Get-AzDevOpsProject.Tests.ps1
@@ -43,6 +43,13 @@ InModuleScope 'AzureDevOpsDsc.Common' {
             $testCasesValidProjectNames) -Expand
         $testCasesValidApiUriPatProjectNames3 = $testCasesValidApiUriPatProjectNames | Select-Object -First 3
 
+        $testCasesValidApiUriPatProjectIdProjectNames = Join-TestCaseArray -TestCaseArray @(
+            $testCasesValidApiUris,
+            $testCasesValidPats,
+            $testCasesValidProjectIds,
+            $testCasesValidProjectNames) -Expand
+        $testCasesValidApiUriPatProjectIdProjectNames3 = $testCasesValidApiUriPatProjectIdProjectNames | Select-Object -First 3
+
         $validProjectIdThatExists = '3456bc8e-0c47-440e-bd49-6db608abb461'
         $validProjectNameThatExists = 'AProjectThatExists'
         $validProjectNameThatExistsByLike = 'AProjectThatExi*'
@@ -66,6 +73,19 @@ InModuleScope 'AzureDevOpsDsc.Common' {
             $testCasesInvalidProjectIds) -Expand
         $testCasesInvalidApiUriPatProjectIds3 = $testCasesInvalidApiUriPatProjectIds | Select-Object -First 3
 
+        $testCasesInvalidProjectNames = Get-TestCase -ScopeName 'ProjectName' -TestCaseName 'Invalid'
+        $testCasesInvalidApiUriPatProjectNames = Join-TestCaseArray -TestCaseArray @(
+            $testCasesInvalidApiUris,
+            $testCasesInvalidPats,
+            $testCasesInvalidProjectNames) -Expand
+        $testCasesInvalidApiUriPatProjectNames3 = $testCasesInvalidApiUriPatProjectNames | Select-Object -First 3
+
+        $testCasesInvalidApiUriPatProjectIdProjectNames = Join-TestCaseArray -TestCaseArray @(
+            $testCasesInvalidApiUris,
+            $testCasesInvalidPats,
+            $testCasesInvalidProjectIds,
+            $testCasesInvalidProjectNames) -Expand
+        $testCasesInvalidApiUriPatProjectIdProjectNames3 = $testCasesInvalidApiUriPatProjectIdProjectNames | Select-Object -First 3
 
 
 
@@ -90,6 +110,7 @@ InModuleScope 'AzureDevOpsDsc.Common' {
 
                     Assert-MockCalled 'Get-AzDevOpsApiResource' -Times 1 -Exactly -Scope 'It'
                 }
+
 
                 Context 'When "Project" resources do exist' {
 
@@ -141,6 +162,7 @@ InModuleScope 'AzureDevOpsDsc.Common' {
                     }
 
                 }
+
 
                 Context 'When "Project" resources do not exist' {
 
@@ -355,7 +377,155 @@ InModuleScope 'AzureDevOpsDsc.Common' {
 
                                 [System.Management.Automation.PSObject]$project = Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectName $validProjectNameThatExistsByLike
 
-                                $project.name | Should -BeLike $validProjectNameThatExists
+                                $project.name | Should -BeLike $validProjectNameThatExistsByLike
+                            }
+                        }
+
+                    }
+
+
+                    Context 'When an "Project" resource does not exist' {
+
+
+                        Context "When using an exact 'ProjectName' that does not exist'" {
+
+                            It 'Should return $null - "<ApiUri>", "<Pat>"' -TestCases $testCasesValidApiUriPats3 {
+                                param ([string]$ApiUri, [string]$Pat)
+
+                                $projects = Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectName $validProjectNameThatDoesNotExist
+                                $projects | Should -BeNullOrEmpty
+                            }
+
+                            It 'Should return no "Project" resources - "<ApiUri>", "<Pat>"' -TestCases $testCasesValidApiUriPats3 {
+                                param ([string]$ApiUri, [string]$Pat)
+
+                                [System.Management.Automation.PSObject[]]$projects = Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectName $validProjectNameThatDoesNotExist
+                                $projects.Count | Should -Be 0
+                            }
+                        }
+
+
+                        Context "When using a wildcard 'ProjectName' that does not exist'" {
+
+                            It 'Should return $null - "<ApiUri>", "<Pat>"' -TestCases $testCasesValidApiUriPats3 {
+                                param ([string]$ApiUri, [string]$Pat)
+
+                                $projects = Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectName $validProjectNameThatDoesNotExistByLike
+                                $projects | Should -BeNullOrEmpty
+                            }
+
+                            It 'Should return no "Project" resources - "<ApiUri>", "<Pat>"' -TestCases $testCasesValidApiUriPats3 {
+                                param ([string]$ApiUri, [string]$Pat)
+
+                                [System.Management.Automation.PSObject[]]$projects = Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectName $validProjectNameThatDoesNotExistByLike
+                                $projects.Count | Should -Be 0
+                            }
+                        }
+                    }
+                }
+
+
+                Context 'When also called with optional, "ProjectId" and "ProjectName" parameters' {
+
+                    It 'Should not throw - "<ApiUri>", "<Pat>", "<ProjectId>", "<ProjectName>"' -TestCases $testCasesValidApiUriPatProjectIdProjectNames {
+                        param ([string]$ApiUri, [string]$Pat, [string]$ProjectId, [string]$ProjectName)
+
+                        { Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectId $ProjectId -ProjectName $ProjectName } | Should -Not -Throw
+                    }
+
+                    It 'Should invoke "Get-AzDevOpsApiResource" only once - "<ApiUri>", "<Pat>", "<ProjectId>", "<ProjectName>"' -TestCases $testCasesValidApiUriPatProjectIdProjectNames3 {
+                        param ([string]$ApiUri, [string]$Pat, [string]$ProjectId, [string]$ProjectName)
+
+                        Mock Get-AzDevOpsApiResource {} -Verifiable
+
+                        Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectId $ProjectId -ProjectName $ProjectName | Out-Null
+
+                        Assert-MockCalled 'Get-AzDevOpsApiResource' -Times 1 -Exactly -Scope 'It'
+                    }
+
+
+                    Context 'When an "Project" resource exists' {
+
+
+                        Mock Get-AzDevOpsApiResource {
+                            [System.Management.Automation.PSObject[]]$projects = @(
+                                $([System.Management.Automation.PSObject]@{
+                                    id = '6c5cfb48-ef00-4965-9e8b-8890cea541b0'
+                                    name = 'SomeProject'
+                                }),
+                                $([System.Management.Automation.PSObject]@{
+                                    id = '3456bc8e-0c47-440e-bd49-6db608abb461' # Same as $validProjectIdThatExists
+                                    name = 'AProjectThatExists'                 # Same as $validProjectNameThatExists
+                                }),
+                                $([System.Management.Automation.PSObject]@{
+                                    id = 'a058fe7e-b336-4d7f-9131-59ab9640bef4'
+                                    name = 'Another Project'
+                                }),
+                                $([System.Management.Automation.PSObject]@{
+                                    id = '9b8dc0c7-36cb-45aa-8177-945583fe253c'
+                                    name = 'Yet Another Project'
+                                }),
+                                $([System.Management.Automation.PSObject]@{
+                                    id = '19aea70c-1339-44b1-b7a3-9d8e6c421a74'
+                                    name = 'The Last Project'
+                                })
+                            )
+
+                            if ($script:mockGetAzDevOpsApiResourceInvoked)
+                            {
+                                $projects = $projects |
+                                    Where-Object { $_.id -eq '3456bc8e-0c47-440e-bd49-6db608abb461' } # Same as $validProjectIdThatExists
+                            }
+                            $script:mockGetAzDevOpsApiResourceInvoked = $true
+
+                            return $projects
+                        } -ModuleName $script:subModuleName
+
+
+                        Context "When using an exact 'ProjectName' that exists'" {
+
+                            It 'Should return exactly 1 "Project" resource - "<ApiUri>", "<Pat>"' -TestCases $testCasesValidApiUriPats3 {
+                                param ([string]$ApiUri, [string]$Pat)
+
+                                $script:mockGetAzDevOpsApiResourceInvoked = $false # For mock of 'Get-AzDevOpsApiResource'
+
+                                [System.Management.Automation.PSObject[]]$projects = Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectId $validProjectIdThatExists -ProjectName $validProjectNameThatExists
+                                $projects.Count | Should -Be 1
+                            }
+
+                            It 'Should return exactly 1 "Project" resource with identical "id" and "name" - "<ApiUri>", "<Pat>"' -TestCases $testCasesValidApiUriPats3 {
+                                param ([string]$ApiUri, [string]$Pat)
+
+                                $script:mockGetAzDevOpsApiResourceInvoked = $false # For mock of 'Get-AzDevOpsApiResource'
+
+                                [System.Management.Automation.PSObject]$project = Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectId $validProjectIdThatExists -ProjectName $validProjectNameThatExists
+
+                                $project.id | Should -Be $validProjectIdThatExists
+                                $project.name | Should -Be $validProjectNameThatExists
+                            }
+                        }
+
+
+                        Context "When using a wildcard 'ProjectName' that exists'" {
+
+                            It 'Should return exactly 1 "Project" resource - "<ApiUri>", "<Pat>"' -TestCases $testCasesValidApiUriPats3 {
+                                param ([string]$ApiUri, [string]$Pat)
+
+                                $script:mockGetAzDevOpsApiResourceInvoked = $false # For mock of 'Get-AzDevOpsApiResource'
+
+                                [System.Management.Automation.PSObject[]]$projects = Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectId $validProjectIdThatExists -ProjectName $validProjectNameThatExistsByLike
+                                $projects.Count | Should -Be 1
+                            }
+
+                            It 'Should return exactly 1 "Project" resource with identical "id" and similar/like "name" - "<ApiUri>", "<Pat>"' -TestCases $testCasesValidApiUriPats3 {
+                                param ([string]$ApiUri, [string]$Pat)
+
+                                $script:mockGetAzDevOpsApiResourceInvoked = $false # For mock of 'Get-AzDevOpsApiResource'
+
+                                [System.Management.Automation.PSObject]$project = Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectId $validProjectIdThatExists -ProjectName $validProjectNameThatExistsByLike
+
+                                $project.id | Should -Be $validProjectIdThatExists
+                                $project.name | Should -BeLike $validProjectNameThatExistsByLike
                             }
                         }
 
@@ -431,6 +601,16 @@ InModuleScope 'AzureDevOpsDsc.Common' {
 
                     It 'Should throw - "<ApiUri>", "<Pat>", "<ProjectName>"' -TestCases $testCasesInvalidApiUriPatProjectNames {
                         param ([string]$ApiUri, [string]$Pat, [string]$ProjectName)
+
+                        { Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectName $ProjectName } | Should -Throw
+                    }
+                }
+
+
+                Context 'When also called with invalid, optional, "ProjectId" and "ProjectName" parameters' {
+
+                    It 'Should throw - "<ApiUri>", "<Pat>", "<ProjectId>", "<ProjectName>"' -TestCases $testCasesInvalidApiUriPatProjectIdProjectNames3 {
+                        param ([string]$ApiUri, [string]$Pat, [string]$ProjectId, [string]$ProjectName)
 
                         { Get-AzDevOpsProject -ApiUri $ApiUri -Pat $Pat -ProjectName $ProjectName } | Should -Throw
                     }
