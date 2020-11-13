260473ec1c1043ef6a910718ab308e6163fb7035
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-13 16:05:17 +0000

    Removed additional references to 'AzureDevOpsDsc.Server' and 'AzureDevOpsDsc.Services' modules within the 'AzureDevOpsDsc.psm1' script.

--- source/AzureDevOpsDsc.psm1
+++ source/AzureDevOpsDsc.psm1
@@ -1,11 +1,7 @@
 $script:azureDevOpsDscCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\AzureDevOpsDsc.Common'
-$script:azureDevOpsDscServerModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\AzureDevOpsDsc.Server'
-$script:azureDevOpsDscServicesModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\AzureDevOpsDsc.Services'
 #$script:dscResourceCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\DscResource.Common'
 
 Import-Module -Name $script:azureDevOpsDscCommonModulePath
-Import-Module -Name $script:azureDevOpsDscServerModulePath
-Import-Module -Name $script:azureDevOpsDscServicesModulePath
 #Import-Module -Name $script:dscResourceCommonModulePath
 
 $script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
