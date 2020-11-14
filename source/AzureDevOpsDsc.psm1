53b8755c8db51ca11555241f5e0c3d9c68d81139
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-14 19:55:00 +0000

    Removed comments in ''AzureDevOpsDsc.psm1'

--- source/AzureDevOpsDsc.psm1
+++ source/AzureDevOpsDsc.psm1
@@ -1,10 +1,6 @@
 
 
 $script:azureDevOpsDscCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\AzureDevOpsDsc.Common'
-#$script:dscResourceCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\DscResource.Common'
-
 Import-Module -Name $script:azureDevOpsDscCommonModulePath
-#Import-Module -Name $script:dscResourceCommonModulePath
 
 $script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
-
