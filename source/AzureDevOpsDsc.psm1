9b94f2837d531c8b5f9b68722ba3162e7bc452dd
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-14 12:19:16 +0000

    Removed 'Ensure' and 'RequiredAction' enums, and 'AzDevOpsDscResource' class from 'AzureDevOpsDsc.psm1' file (and added 'using' references).

--- source/AzureDevOpsDsc.psm1
+++ source/AzureDevOpsDsc.psm1
@@ -1,3 +1,8 @@
+using module .\Enums\Ensure\Ensure.psm1
+using module .\Enums\RequiredAction\RequiredAction.psm1
+using module .\Classes\AzDevOpsDscResource\AzDevOpsDscResource.psm1
+
+
 $script:azureDevOpsDscCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\AzureDevOpsDsc.Common'
 #$script:dscResourceCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\DscResource.Common'
 
@@ -7,105 +12,6 @@ Import-Module -Name $script:azureDevOpsDscCommonModulePath
 $script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
 
 
-enum Ensure
-{
-    Present
-    Absent
-}
-
-enum RequiredAction
-{
-    None
-    Get
-    New
-    Set
-    Remove
-    Test
-    Error
-}
-
-
-
-class AzDevOpsDscResource
-{
-    hidden [System.String]GetDscResourceKey()
-    {
-        [System.String]$dscResourceKeyPropertyName = $this.GetDscResourceKeyPropertyName()
-
-        if ([System.String]::IsNullOrWhiteSpace($dscResourceKeyPropertyName))
-        {
-            return $null
-        }
-
-        return $this."$dscResourceKeyPropertyName"
-    }
-
-    hidden [System.String]GetDscResourceKeyPropertyName()
-    {
-        [System.String[]]$dscResourceKeyPropertyNames = @()
-
-        [Type]$thisType = $this.GetType()
-        [System.Reflection.PropertyInfo[]]$thisProperties = $thisType.GetProperties()
-
-        $thisProperties | ForEach-Object {
-
-            [System.Reflection.PropertyInfo]$propertyInfo = $_
-            $PropertyName = $_.Name
-
-            $propertyInfo.GetCustomAttributes($true) |
-            ForEach-Object {
-
-                if ($_.TypeId.Name -eq 'DscPropertyAttribute' -and
-                    $_.Key -eq $true)
-                {
-                    $dscResourceKeyPropertyNames += $PropertyName
-                }
-            }
-        }
-
-        if ($null -eq $dscResourceKeyPropertyNames -or $dscResourceKeyPropertyNames.Count -eq 0)
-        {
-            throw "Could not obtain a 'DscResourceDscKey' property for type '$($this.GetType().Name)'."
-        }
-        elseif ($dscResourceKeyPropertyNames.Count -gt 1)
-        {
-            throw "Obtained more than 1 property for type '$($this.GetType().Name)' that was marked as a 'Key'. There must only be 1 property on the class set as the 'Key' for DSC."
-        }
-
-        return $dscResourceKeyPropertyNames[0]
-    }
-
-
-    hidden [System.String[]]GetDscResourcePropertyNames()
-    {
-        [System.String[]]$thisDscPropertyNames = @()
-
-        [Type]$thisType = $this.GetType()
-        [System.Reflection.PropertyInfo[]]$thisProperties = $thisType.GetProperties()
-
-        $thisProperties | ForEach-Object {
-            $propertyInfo = $_
-            $PropertyName = $_.Name
-
-            $propertyInfo.GetCustomAttributes($true) |
-            ForEach-Object {
-
-                if ($_.TypeId.Name -eq 'DscPropertyAttribute')
-                {
-                    $thisDscPropertyNames += $PropertyName
-                }
-            }
-        }
-
-        return $thisDscPropertyNames
-    }
-
-    hidden [System.String[]]GetDscResourcePropertyNamesWithNoSetSupport()
-    {
-        return @()
-    }
-
-}
 
 class AzDevOpsApiDscResource : AzDevOpsDscResource
 {
