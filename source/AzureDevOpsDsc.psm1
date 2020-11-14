9dcffddd42eafeb426edf06a64b0d1c8dfaaa40c
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-14 15:55:39 +0000

    Moved 'AzDevOpsProject', DSC Resource into it's own file/module and out of 'AzureDevOpsDsc.psm1' file.

--- source/AzureDevOpsDsc.psm1
+++ source/AzureDevOpsDsc.psm1
@@ -1,6 +1,3 @@
-using module .\Classes\DscResourceBase\DscResourceBase.psm1
-using module .\Classes\AzDevOpsApiDscResourceBase\AzDevOpsApiDscResourceBase.psm1
-using module .\Classes\AzDevOpsDscResourceBase\AzDevOpsDscResourceBase.psm1
 
 
 $script:azureDevOpsDscCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\AzureDevOpsDsc.Common'
@@ -11,59 +8,3 @@ Import-Module -Name $script:azureDevOpsDscCommonModulePath
 
 $script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
 
-
-[DscResource()]
-[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSDSCStandardDSCFunctionsInResource', '', Justification='Test() and Set() method are inherited from base, "AzDevOpsDscResourceBase" class')]
-class AzDevOpsProject : AzDevOpsDscResourceBase
-{
-    [DscProperty()]
-    [Alias('Id')]
-    [System.String]$ProjectId
-
-    [DscProperty(Key, Mandatory)]
-    [Alias('Name')]
-    [System.String]$ProjectName
-
-    [DscProperty()]
-    [Alias('Description')]
-    [System.String]$ProjectDescription
-
-    [DscProperty()]
-    [System.String]$SourceControlType
-
-
-    [AzDevOpsProject] Get()
-    {
-        return [AzDevOpsProject]$($this.GetDscCurrentStateProperties())
-    }
-
-
-    hidden [System.String[]]GetDscResourcePropertyNamesWithNoSetSupport()
-    {
-        return @('SourceControlType')
-    }
-
-    hidden [Hashtable]GetDscCurrentStateProperties([PSCustomObject]$CurrentResourceObject)
-    {
-        $properties = @{
-            Pat = $this.Pat
-            ApiUri = $this.ApiUri
-            Ensure = [Ensure]::Absent
-        }
-
-        if ($null -ne $CurrentResourceObject)
-        {
-            if (![System.String]::IsNullOrWhiteSpace($CurrentResourceObject.id))
-            {
-                $properties.Ensure = [Ensure]::Present
-            }
-            $properties.ProjectId = $CurrentResourceObject.id
-            $properties.ProjectName = $CurrentResourceObject.name
-            $properties.ProjectDescription = $CurrentResourceObject.description
-            $properties.SourceControlType = $CurrentResourceObject.capabilities.versioncontrol.sourceControlType
-        }
-
-        return $properties
-    }
-
-}
