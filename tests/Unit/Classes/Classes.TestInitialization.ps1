052d20a7d3917e273eb15c4561839e8065c8ed96
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-14 21:22:37 +0000

    Updated test-initialization scripts to be identical

--- tests/Unit/Classes/Classes.TestInitialization.ps1
+++ tests/Unit/Classes/Classes.TestInitialization.ps1
@@ -4,14 +4,19 @@
 #>
 
 Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '\..\Modules\TestHelpers\CommonTestHelper.psm1')
-
-if (-not (Test-BuildCategory -Type 'Unit'))
-{
-    return
-}
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '\..\Modules\TestHelpers\CommonTestCases.psm1')
 
 $script:dscModuleName = 'AzureDevOpsDsc'
 $script:dscModule = Get-Module -Name $script:dscModuleName -ListAvailable | Select-Object -First 1
 $script:dscModuleFile = $($script:dscModule.ModuleBase +'\'+ $script:dscModuleName + ".psd1")
+Get-Module -Name $script:dscModuleName -All |
+    Remove-Module $script:dscModuleName -Force -ErrorAction SilentlyContinue
+
 $script:subModuleName = 'AzureDevOpsDsc.Common'
-Import-Module -Name $script:dscModuleFile -Force -Verbose
+Import-Module -Name $script:dscModuleFile -Force
+
+Get-Module -Name $script:subModuleName -All |
+    Remove-Module $script:subModuleName -Force -ErrorAction SilentlyContinue
+$script:subModulesFolder = Join-Path -Path $script:dscModule.ModuleBase -ChildPath 'Modules'
+$script:subModuleFile = Join-Path $script:subModulesFolder "$($script:subModuleName)/$($script:subModuleName).psd1"
+#Import-Module -Name $script:subModuleFile -Force #-Verbose
