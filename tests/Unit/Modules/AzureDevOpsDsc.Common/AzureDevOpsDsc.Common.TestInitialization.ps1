e5b52e7f18c06de67bdc954897bb197c04c69b96
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-14 22:58:18 +0000

    Updated test initialization script

--- tests/Unit/Modules/AzureDevOpsDsc.Common/AzureDevOpsDsc.Common.TestInitialization.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/AzureDevOpsDsc.Common.TestInitialization.ps1
@@ -1,22 +1,22 @@
 <#
     .SYNOPSIS
-        Automated unit test for helper functions in module AzureDevOpsDsc.Common.
+        Automated unit test for classes in AzureDevOpsDsc.
 #>
 
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestHelper.psm1') -Force
-Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '..\TestHelpers\CommonTestCases.psm1') -Force
-
-if (-not (Test-BuildCategory -Type 'Unit'))
-{
-    return
-}
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '\..\Modules\TestHelpers\CommonTestHelper.psm1')
+Import-Module -Name (Join-Path -Path $PSScriptRoot -ChildPath '\..\Modules\TestHelpers\CommonTestCases.psm1')
 
 $script:dscModuleName = 'AzureDevOpsDsc'
 $script:dscModule = Get-Module -Name $script:dscModuleName -ListAvailable | Select-Object -First 1
 $script:dscModuleFile = $($script:dscModule.ModuleBase +'\'+ $script:dscModuleName + ".psd1")
-$script:subModuleName = 'AzureDevOpsDsc.Common'
-Import-Module -Name $script:dscModuleFile -Force -Verbose
+Get-Module -Name $script:dscModuleName -All |
+    Remove-Module $script:dscModuleName -Force -ErrorAction SilentlyContinue
 
+$script:subModuleName = 'AzureDevOpsDsc.Common'
+Import-Module -Name $script:dscModuleFile -Force
+
+Get-Module -Name $script:subModuleName -All |
+    Remove-Module -Force -ErrorAction SilentlyContinue
 $script:subModulesFolder = Join-Path -Path $script:dscModule.ModuleBase -ChildPath 'Modules'
 $script:subModuleFile = Join-Path $script:subModulesFolder "$($script:subModuleName)/$($script:subModuleName).psd1"
-Import-Module -Name $script:subModuleFile -Force -Verbose
+Import-Module -Name $script:subModuleFile -Force #-Verbose
