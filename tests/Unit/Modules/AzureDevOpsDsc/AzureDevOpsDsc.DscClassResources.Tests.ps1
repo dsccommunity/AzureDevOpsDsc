f2e746b7b39c6c64281344e87a62e85d9bfdd997
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-15 10:38:14 +0000

    Removed/Suppressed module/generic, 'DscClassResource' tests

--- tests/Unit/Modules/AzureDevOpsDsc/AzureDevOpsDsc.DscClassResources.Tests.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc/AzureDevOpsDsc.DscClassResources.Tests.ps1
@@ -1,30 +1,30 @@
 
-# Initialize tests
-. $PSScriptRoot\AzureDevOpsDsc.TestInitialization.ps1
+# # Initialize tests
+# . $PSScriptRoot\AzureDevOpsDsc.TestInitialization.ps1
 
 
-InModuleScope 'AzureDevOpsDsc' {
+# InModuleScope 'AzureDevOpsDsc' {
 
-    Describe 'DSCClassResources\AzDevOpsApiDscResourceBase' -Tag 'AzDevOpsApiDscResourceBase' {
+#     Describe 'DSCClassResources\AzDevOpsApiDscResourceBase' -Tag 'AzDevOpsApiDscResourceBase' {
 
-        $dscModuleName = 'AzureDevOpsDsc'
-        $testCasesValidResourceNames = Get-TestCase -ScopeName 'ResourceName' -TestCaseName 'Valid'
-        $testCasesValidResourceNamesForDscResources = $testCasesValidResourceNames | Where-Object { $_.ResourceName -notin @('Operation')}
+#         $dscModuleName = 'AzureDevOpsDsc'
+#         $testCasesValidResourceNames = Get-TestCase -ScopeName 'ResourceName' -TestCaseName 'Valid'
+#         $testCasesValidResourceNamesForDscResources = $testCasesValidResourceNames | Where-Object { $_.ResourceName -notin @('Operation')}
 
-        Context "When evaluating '$dscModuleName' module" {
-            BeforeAll {
-                $dscModuleName = 'AzureDevOpsDsc'
-                $dscResourcePrefix = 'AzDevOps'
-                [string[]]$exportedDscResources = (Get-Module $dscModuleName).ExportedDscResources
-            }
+#         Context "When evaluating '$dscModuleName' module" {
+#             BeforeAll {
+#                 $dscModuleName = 'AzureDevOpsDsc'
+#                 $dscResourcePrefix = 'AzDevOps'
+#                 [string[]]$exportedDscResources = (Get-Module $dscModuleName).ExportedDscResources
+#             }
 
-            It "Should contain an exported, DSCResource specific to the 'ResourceName' - '<ResourceName>'" -TestCases $testCasesValidResourceNamesForDscResources {
-                param ([string]$ResourceName)
+#             It "Should contain an exported, DSCResource specific to the 'ResourceName' - '<ResourceName>'" -TestCases $testCasesValidResourceNamesForDscResources {
+#                 param ([string]$ResourceName)
 
-                "$dscResourcePrefix$ResourceName" | Should -BeIn $exportedDscResources
-            }
+#                 "$dscResourcePrefix$ResourceName" | Should -BeIn $exportedDscResources
+#             }
 
-        }
+#         }
 
-    }
-}
+#     }
+# }
