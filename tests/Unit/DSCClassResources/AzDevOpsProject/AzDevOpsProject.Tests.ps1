7f1b0e011199d10c9ef0729c6e07ea493e0b1436
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-15 13:43:51 +0000

    Added initial, 'AzDevOpsProject', unit tests for Get() method.

--- tests/Unit/DSCClassResources/AzDevOpsProject/AzDevOpsProject.Tests.ps1
+++ tests/Unit/DSCClassResources/AzDevOpsProject/AzDevOpsProject.Tests.ps1
@@ -1,3 +1,45 @@
+# Initialize tests for module function
+. $PSScriptRoot\..\DSCClassResources.TestInitialization.ps1
+
+InModuleScope 'AzureDevOpsDsc' {
+
+    $script:dscModuleName = 'AzureDevOpsDsc'
+    $script:moduleVersion = $(Get-Module -Name $script:dscModuleName -ListAvailable | Select-Object -First 1).Version
+    $script:subModuleName = 'AzureDevOpsDsc.Common'
+    $script:subModuleBase = $(Get-Module $script:subModuleName).ModuleBase
+    $script:dscResourceName = Split-Path $PSScriptRoot -Leaf
+    $script:commandName = $(Get-Item $PSCommandPath).BaseName.Replace('.Tests','')
+    $script:commandScriptPath = Join-Path "$PSScriptRoot\..\..\..\..\" -ChildPath "output\$($script:dscModuleName)\$($script:moduleVersion)\Classes\$script:dscResourceName\$script:dscResourceName.psm1"
+    $script:tag = @($($script:commandName -replace '-'))
+
+
+    Describe "$script:subModuleName\Classes\DscResourceBase\Method\$script:commandName" -Tag $script:tag {
+
+        Context 'When creating a new instance of the class' {
+
+            It 'Should not throw' {
+
+                {[AzDevOpsProject]::new()} | Should -Not -Throw
+            }
+        }
+
+        Context 'When creating a new instance of the class' {
+
+            It 'Should not throw' {
+
+                {[AzDevOpsProject]::new()} | Should -Not -Throw
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
 # <#
 #     .SYNOPSIS
 #         Automated unit test for AzDevOpsProject DSC Resource.
