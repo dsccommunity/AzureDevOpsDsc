1e30e2664dab78f760b826c7ff4d09e0b205b0ed
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-19 17:39:10 +0000

    Moved 'AzureDevOpsDsc.psm1' contents into 'prefix.ps1'

--- source/AzureDevOpsDsc.psm1
+++ source/AzureDevOpsDsc.psm1
@@ -1,6 +1,2 @@
 
 
-$script:azureDevOpsDscCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\AzureDevOpsDsc.Common'
-Import-Module -Name $script:azureDevOpsDscCommonModulePath
-
-$script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
