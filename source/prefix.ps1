3ed89dc80ef04925ad260d5eb936a2ae5e699bfa
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2020-11-19 18:26:10 +0000

    Minor reformatting of 'prefix.ps1' and 'suffix.ps1'

--- source/prefix.ps1
+++ source/prefix.ps1
@@ -1,19 +1,20 @@
-# '.psm1' Prefix
 
+
+# Import nested, 'AzureDevOpsDsc.Common' module
 $script:azureDevOpsDscCommonModulePath = Join-Path -Path $PSScriptRoot -ChildPath 'Modules\AzureDevOpsDsc.Common'
 Import-Module -Name $script:azureDevOpsDscCommonModulePath
 
+
+# Define localization data
 $script:localizedData = Get-LocalizedData -DefaultUICulture 'en-US'
 
 
-
+# Define 'enums' for module
 enum Ensure
 {
     Present
     Absent
 }
-
-
 
 enum RequiredAction
 {
@@ -25,3 +26,4 @@ enum RequiredAction
     Test
     Error
 }
+
