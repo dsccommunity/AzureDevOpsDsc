aaa1840494718d7238fac7041b74000733c6ace9
Michael Zanatta <michael.zanatta@mail.com>
2024-02-25 01:55:50 +0000

    Remove comments from -Force

--- tests/Unit/Modules/AzureDevOpsDsc.Common/AzureDevOpsDsc.Common.TestInitialization.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/AzureDevOpsDsc.Common.TestInitialization.ps1
@@ -36,10 +36,10 @@ Get-Module -Name $script:dscModuleName -All |
     Remove-Module $script:dscModuleName -Force -ErrorAction SilentlyContinue
 
 $script:subModuleName = 'AzureDevOpsDsc.Common'
-Import-Module -Name $script:dscModuleFile #-Force
+Import-Module -Name $script:dscModuleFile -Force
 
 Get-Module -Name $script:subModuleName -All |
     Remove-Module -Force -ErrorAction SilentlyContinue
 $script:subModulesFolder = Join-Path -Path $script:dscModule.ModuleBase -ChildPath 'Modules'
 $script:subModuleFile = Join-Path $script:subModulesFolder "$($script:subModuleName)/$($script:subModuleName).psd1"
-Import-Module -Name $script:subModuleFile #-Force #-Verbose
+Import-Module -Name $script:subModuleFile -Force #-Verbose
