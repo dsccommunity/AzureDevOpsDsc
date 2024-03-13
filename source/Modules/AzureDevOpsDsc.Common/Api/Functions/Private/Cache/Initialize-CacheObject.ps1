e28f5149414a0df3ffa3295346e5c978ae463584
Michael Zanatta <michael.zanatta@mail.com>
2024-03-13 21:44:10 +0000

    Fixing Bugs with CacheObject Updating GitIgnore

--- source/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Cache/Initialize-CacheObject.ps1
+++ source/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Cache/Initialize-CacheObject.ps1
@@ -24,18 +24,24 @@ Function Initialize-CacheObject {
 
     try {
 
+        $cacheFilePath = Join-Path -Path $PSScriptRoot -ChildPath "Cache\$CacheType.clixml"
+        Write-Verbose "[Initialize-CacheObject] Cache file path: $cacheFilePath"
+
         # If the cache group is LiveGroups or LiveProjects, set the cache file path to the temporary directory
-        if ($CacheType -eq 'LiveGroups' -or $CacheType -eq 'LiveProjects') {
-            $cacheFilePath = Join-Path -Path $env:TEMP -ChildPath "$CacheType.clixml"
-            Write-Verbose "[Initialize-CacheObject] Cache file path: $cacheFilePath"
-        } else {
-            $cacheFilePath = Join-Path -Path $PSScriptRoot -ChildPath "Cache\.clixml"
-            Write-Verbose "[Initialize-CacheObject] Cache file path: $cacheFilePath"
+        if (($CacheType -eq 'LiveGroups') -or ($CacheType -eq 'LiveProjects')) {
+
+            # Flush the cache if it is a live cache
+            if (Test-Path -LiteralPath $cacheFilePath -ErrorAction SilentlyContinue) {
+                Write-Verbose "[Initialize-CacheObject] Cache file found. Removing cache file for '$CacheType'."
+                Remove-Item -LiteralPath $cacheFilePath -Force
+            }
+
         }
 
         # Test if the Cache File exists. If it does, import the cache object
         if (Test-Path -Path $cacheFilePath) {
 
+            # If the cache file exists, import the cache object
             Write-Verbose "[Initialize-CacheObject] Cache file found. Importing cache object for '$CacheType'."
             Import-CacheObject @PSBoundParameters
 
