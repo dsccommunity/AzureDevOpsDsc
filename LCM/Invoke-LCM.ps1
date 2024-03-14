31249652def40905f3e5811ddc46320194c13cc8
Michael Zanatta <michael.zanatta@mail.com>
2024-03-14 04:52:04 +0000

    Adding Managed Identity Tooling

--- LCM/Invoke-LCM.ps1
+++ LCM/Invoke-LCM.ps1
@@ -1,5 +1,8 @@
 #
-# Update the Cache
+# Craft a new Azure DevOps Managed Identity
+# This sets the $Global:DSCAZDO_OrganizationName variable
+
+New-AzManagedIdentity -OrganizationName "Contoso"
 
 # Set the Group Cache
 Set-AzDoAPIGroupCache -OrganizationName $Global:DSCAZDO_OrganizationName
