8811019ee603e248d5e382c51510823b93656970
Michael Zanatta <zanattaam@gmail.com>
2024-04-10 20:00:42 +1000

    Update Configuration

--- LCM/build.ps1
+++ LCM/build.ps1
@@ -1,3 +1,8 @@
+Import-Module 'C:\Temp\AzureDevOpsDSC\LCM\Datum\powershell-yaml\0.4.7\powershell-yaml.psd1'
+Import-Module 'C:\Temp\AzureDevOpsDSC\LCM\Datum\datum\0.40.1\datum.psd1'
+
+Set-Location 'C:\Temp\AzureDevOpsDSC\Example Configuration'
+
 $Project = 'TestProject'
 $Datum = New-DatumStructure -DefinitionFile Datum.yml
 
@@ -16,5 +21,8 @@ $a = (Lookup MergeTest1)
 
 $TestProject = $Datum.AllNodes.TestProject.NodeGroups
 Lookup 'project' -Node $TestProject -DatumTree $Datum
+Lookup 'Resources' -Node $TestProject -DatumTree $Datum
+
+Resolve-Datum -SearchPaths $datum.__Definition.ResolutionPrecedence -DatumStructure $Datum -PropertyPath 'Resources'
 
 $r = Get-DatumRsop -Datum $Datum -AllNodes $Node -Verbose
