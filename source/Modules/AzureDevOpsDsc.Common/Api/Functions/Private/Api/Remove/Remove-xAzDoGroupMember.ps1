d6990d1f81d7a6376a59a2e0d56a415efe30b789
Michael Zanatta <zanattaam@gmail.com>
2024-06-04 11:39:51 +1000

    Add Remove-xAzDoGroupMember

---
+++ source/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Api/Remove/Remove-xAzDoGroupMember.ps1
@@ -0,0 +1,62 @@
+Function Remove-DevOpsGroupMember {
+    [CmdletBinding()]
+    param
+    (
+        # The group Identity
+        [Parameter(Mandatory)]
+        [Alias('Group')]
+        [Object]$GroupIdentity,
+
+        # The group member
+        [Parameter(Mandatory)]
+        [Alias('Member')]
+        [Object]$MemberIdentity,
+
+        # Optional parameter for the API version with a default value obtained from the Get-AzDevOpsApiVersion function
+        [Parameter()]
+        [String]
+        $ApiVersion = $(Get-AzDevOpsApiVersion -Default),
+
+        # The URI for the Azure DevOps API.
+        [Parameter(Mandatory)]
+        [string]
+        $ApiUri
+
+    )
+
+    # Define a hashtable to store parameters for the Invoke-AzDevOpsApiRestMethod function.
+    $params = @{
+        # Construct the Uri using string formatting with the -f operator.
+        # It includes the API endpoint, group identity, member identity, and the API version.
+        Uri = "{0}/_apis/graph/memberships/{1}/{2}?api-version={3}" -f  $ApiUri,
+                                                                        $MemberIdentity.descriptor,
+                                                                        $GroupIdentity.descriptor,
+                                                                        $ApiVersion
+        # Specifies the HTTP method to be used in the REST call, in this case 'PUT'.
+        Method = 'DELETE'
+    }
+
+    Write-Verbose "[Remove-DevOpsGroupMember] Constructed URI for REST call: $($params.Uri)"
+
+    # Try to invoke the REST method to create the group and return the result
+
+    try {
+        # Call the Invoke-AzDevOpsApiRestMethod function with the parameters defined above.
+        # The "@" symbol is used to pass the hashtable as splatting parameters.
+        Write-Verbose "[Remove-DevOpsGroupMember] Attempting to invoke REST method to add group member."
+        $member = Invoke-AzDevOpsApiRestMethod @params
+        Write-Verbose "[Remove-DevOpsGroupMember] Member added successfully."
+
+    } catch {
+        # If an exception occurs, write an error message to the console with details about the issue.
+        Write-Error "[Remove-DevOpsGroupMember] Failed to add member to group: $($_.Exception.Message)"
+    }
+
+    Write-Verbose "[Remove-DevOpsGroupMember] Result $($member | ConvertTo-Json)."
+
+    # Return the result of the REST method invocation, which is stored in $member.
+    Write-Verbose "[Remove-DevOpsGroupMember] Returning result from REST method invocation."
+    return $member
+
+
+}
--- source/Modules/AzureDevOpsDsc.Common/Resources/Functions/Public/xAzDoGroupMember/Remove-xAzDoGroupMember.ps1
+++ source/Modules/AzureDevOpsDsc.Common/Resources/Functions/Public/xAzDoGroupMember/Remove-xAzDoGroupMember.ps1
@@ -4,7 +4,6 @@ Function Remove-xAzDoGroupMember {
     [OutputType([System.Management.Automation.PSObject[]])]
     param
     (
-
         [Parameter(Mandatory)]
         [Alias('Name')]
         [System.String]$GroupName,
@@ -14,7 +13,6 @@ Function Remove-xAzDoGroupMember {
         [System.String[]]$GroupMembers=@(),
 
         [Parameter()]
-        [Alias('Lookup')]
         [HashTable]$LookupResult,
 
         [Parameter()]
@@ -23,10 +21,61 @@ Function Remove-xAzDoGroupMember {
         [Parameter()]
         [System.Management.Automation.SwitchParameter]
         $Force
+
     )
 
-    "TRIGGERED" | Out-File "C:\Temp\New-xAzDoGroupMember.txt"
+    # Fetch the Group Identity
+    $GroupIdentity = Find-AzDoIdentity $GroupName
 
-    $return
+    # Retrieve the group members from the cache
+    $CachedGroupMembers = Get-CacheObject -CacheType 'LiveGroupMembers'
+
+    $params = @{
+        GroupIdentity = $GroupIdentity
+        ApiUri = 'https://vssps.dev.azure.com/{0}/' -f $Global:DSCAZDO_OrganizationName
+    }
+
+    Write-Verbose "[Remove-xAzDoGroupMember] Starting group member removal process for group '$GroupName'."
+    Write-Verbose "[Remove-xAzDoGroupMember] Group members: $($GroupMembers -join ',')."
+
+    # Define the members
+    $members = [System.Collections.Generic.List[object]]::new()
+
+    # Fetch the group members and perform a lookup of the members
+    ForEach ($MemberIdentity in $GroupMembers) {
+
+        # Use the Find-AzDoIdentity function to search for an Azure DevOps identity that matches the given $MemberIdentity.
+        Write-Verbose "[Remove-xAzDoGroupMember] Looking up identity for member '$MemberIdentity'."
+        $identity = Find-AzDoIdentity -Identity $MemberIdentity
+
+        # If the identity is not found, write a warning message to the console and continue to the next member.
+        if ($null -eq $identity) {
+            Write-Warning "[Remove-xAzDoGroupMember] Unable to find identity for member '$MemberIdentity'."
+            continue
+        }
+        Write-Verbose "[Remove-xAzDoGroupMember] Found identity for member '$MemberIdentity'."
+
+        # Call the New-DevOpsGroupMember function with a hashtable of parameters to add the found identity as a new member to a group.
+        Write-Verbose "[Remove-xAzDoGroupMember] Removing member '$MemberIdentity' to group '$($params.GroupIdentity.displayName)'."
+
+        $result = Remove-DevOpsGroupMember @params -MemberIdentity $identity
+
+    }
+
+    # If the group members are not found, write a warning message to the console and return.
+    if ($members.Count -eq 0) {
+        Write-Warning "[Remove-xAzDoGroupMember] No group members found: $($GroupMembers -join ',')."
+        return
+    }
+
+    # Add the group to the cache
+    Write-Verbose "[Remove-xAzDoGroupMember] Added group '$GroupName' with members to the cache."
+    Remove-CacheItem -Key $GroupIdentity.principalName -Type 'LiveGroupMembers'
+
+    Write-Verbose "[Remove-xAzDoGroupMember] Updated global cache with live group information."
+    Set-CacheObject -Content $Global:AZDOLiveGroups -CacheType 'LiveGroupMembers'
+
+    # Write a verbose log message indicating that the function has completed the group member removal process.
+    Write-Verbose "[Remove-xAzDoGroupMember] Completed group member removal process for group '$GroupName'."
 
 }
