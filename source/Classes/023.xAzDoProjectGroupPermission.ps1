c002cd5307c88e77412abfd86429b99a97fa4c51
Michael Zanatta <zanattaam@gmail.com>
2024-07-16 12:52:23 +1000

    Bug Fixes

--- source/Classes/023.xAzDoProjectGroupPermission.ps1
+++ source/Classes/023.xAzDoProjectGroupPermission.ps1
@@ -1,5 +1,58 @@
+<#
+.SYNOPSIS
+    This class represents a DSC resource for managing Azure DevOps project group permissions.
 
-#[DscResource()]
+.DESCRIPTION
+    The xAzDoProjectGroupPermission class is a DSC resource that allows you to manage permissions for Azure DevOps project groups.
+    It inherits from the AzDevOpsDscResourceBase class and provides properties and methods to configure project group permissions.
+
+.PARAMETER GroupName
+    Specifies the name of the Azure DevOps project group.
+    This parameter is mandatory.
+
+.PARAMETER ProjectName
+    Specifies the name of the Azure DevOps project.
+    This parameter is mandatory.
+
+.PARAMETER isInherited
+    Specifies whether the permissions are inherited from parent groups.
+    The default value is $true.
+
+.PARAMETER Permission
+    Specifies the permissions to be assigned to the project group.
+    This parameter is mandatory and should be a hashtable.
+
+.METHODS
+    Get()
+        Retrieves the current state of the xAzDoProjectGroupPermission resource.
+
+.NOTES
+    This class is a DSC resource and should be used with the Azure DevOps DSC module.
+
+.LINK
+    https://github.com/Azure/AzureDevOpsDSC
+
+.EXAMPLE
+    Configuration Example
+    {
+        Import-DscResource -ModuleName AzureDevOpsDSC
+
+        Node localhost
+        {
+            xAzDoProjectGroupPermission MyProjectGroupPermission
+            {
+                GroupName = 'MyGroup'
+                ProjectName = 'MyProject'
+                Permission = @{
+                    'Read' = $true
+                    'Write' = $true
+                }
+                Ensure = 'Present'
+            }
+        }
+    }
+#>
+[DscResource()]
 [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSDSCStandardDSCFunctionsInResource', '', Justification='Test() and Set() method are inherited from base, "AzDevOpsDscResourceBase" class')]
 class xAzDoProjectGroupPermission : AzDevOpsDscResourceBase
 {
