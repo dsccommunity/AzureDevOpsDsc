477b92accacf87a5cdf93723c1c216a2389d02f3
Michael Zanatta <zanattaam@gmail.com>
2024-08-23 12:03:14 +1000

    Adding Tests

--- tests/Unit/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Cache/Cache Initalization/8.ProcessTemplates.tests.ps1
+++ tests/Unit/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Cache/Cache Initalization/8.ProcessTemplates.tests.ps1
@@ -1,21 +1,36 @@
+$currentFile = $MyInvocation.MyCommand.Path
+
 Describe 'AzDoAPI_8_ProjectProcessTemplates' {
-    $mockOrganizationName = 'TestOrg'
-    $mockProcesses = @(
-        [PSCustomObject]@{ name = 'Process1' },
-        [PSCustomObject]@{ name = 'Process2' }
-    )
 
     BeforeAll {
-        Export-ModuleMember -Function AzDoAPI_8_ProjectProcessTemplates
+
+        # Load the functions to test
+        if ($null -eq $currentFile) {
+            $currentFile = Join-Path -Path $PSScriptRoot -ChildPath '8.ProcessTemplates.tests'
+        }
+
+        # Load the functions to test
+        $files = Invoke-BeforeEachFunctions (Find-Functions -TestFilePath $currentFile)
+        ForEach ($file in $files) {
+            . $file.FullName
+        }
+
+        $mockOrganizationName = 'TestOrg'
+        $mockProcesses = @(
+            [PSCustomObject]@{ name = 'Process1' },
+            [PSCustomObject]@{ name = 'Process2' }
+        )
+
         $script:global:DSCAZDO_OrganizationName = 'DefaultOrg'
-    }
 
-    Mock List-DevOpsProcess {
-        param($param) $mockProcesses
-    }
+        Mock -CommandName List-DevOpsProcess -MockWith {
+            param($param) $mockProcesses
+        }
 
-    Mock Add-CacheItem
-    Mock Export-CacheObject
+        Mock -CommandName Add-CacheItem
+        Mock -CommandName Export-CacheObject
+
+    }
 
     Context 'When OrganizationName parameter is provided' {
         It 'should call List-DevOpsProcess with provided OrganizationName' {
@@ -28,7 +43,11 @@ Describe 'AzDoAPI_8_ProjectProcessTemplates' {
             AzDoAPI_8_ProjectProcessTemplates -OrganizationName $mockOrganizationName
 
             $mockProcesses | ForEach-Object {
-                Assert-MockCalled Add-CacheItem -ParameterFilter { $Key -eq $_.name -and $Value -eq $_ -and $Type -eq 'LiveProcesses' } -Exactly 1
+                Assert-MockCalled Add-CacheItem -ParameterFilter {
+                    $Key -eq $_.name -and
+                    $Value -eq $_ -and
+                    $Type -eq 'LiveProcesses'
+                } -Exactly 1
             }
         }
 
@@ -49,13 +68,12 @@ Describe 'AzDoAPI_8_ProjectProcessTemplates' {
 
     Context 'When an error occurs during function execution' {
         It 'should catch and handle the error' {
-            Mock List-DevOpsProcess { throw 'An error occurred' }
-            Mock Write-Error
+            Mock -CommandName List-DevOpsProcess -MockWith { throw 'An error occurred' }
+            Mock -CommandName Write-Error -Verifiable
 
             AzDoAPI_8_ProjectProcessTemplates -OrganizationName $mockOrganizationName
 
-            Assert-MockCalled Write-Error -ParameterFilter { $_.Message -eq 'An error occurred' } -Exactly 1
+            Assert-VerifiableMock
         }
     }
 }
-
