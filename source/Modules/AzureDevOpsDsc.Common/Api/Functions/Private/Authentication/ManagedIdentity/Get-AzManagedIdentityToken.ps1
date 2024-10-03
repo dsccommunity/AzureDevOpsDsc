11f3ba22d7916ed4336bc5256ae78eb514c0e5db
david <david@gmail.com>
2024-10-04 08:03:52 +1000

    Add MI support for Azure Arc

--- source/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Authentication/ManagedIdentity/Get-AzManagedIdentityToken.ps1
+++ source/Modules/AzureDevOpsDsc.Common/Api/Functions/Private/Authentication/ManagedIdentity/Get-AzManagedIdentityToken.ps1
@@ -44,12 +44,36 @@ Function Get-AzManagedIdentityToken {
         Headers = @{ Metadata="true" }
         ContentType = 'Application/json'
         NoAuthentication = $true
+        UseBasicParsing = $true
     }
 
     Write-Verbose "[Get-AzManagedIdentityToken] Invoking the Azure Instance Metadata Service to get the access token."
 
     # Invoke the RestAPI
-    try { $response = Invoke-AzDevOpsApiRestMethod @ManagedIdentityParams } catch { Throw $_ }
+    try {
+        $response = Invoke-AzDevOpsApiRestMethod @ManagedIdentityParams
+    } catch {
+
+        # If there is an error it could be because it's an arc machine, and we need to use the secret file:
+        $wwwAuthHeader = $_.Exception.Response.Headers["WWW-Authenticate"]
+        if ($wwwAuthHeader -notmatch "Basic realm=.+")
+        {
+            Throw $_
+        }
+
+        Write-Verbose "[Get-AzManagedIdentityToken] Managed Identity Token Retrival Failed. Retrying with secret file."
+
+        # Extract the secret file path from the WWW-Authenticate header
+        $secretFile = ($wwwAuthHeader -split "Basic realm=")[1]
+        # Read the secret file to get the token
+        $token = Get-Content -LiteralPath $secretFile -Raw
+        # Add the token to the headers
+        $ManagedIdentityParams.Authorization = "Basic $token"
+
+        # Retry the request. Silently continue to suppress the error message, since we will handle it below.
+        $response = Invoke-AzDevOpsApiRestMethod @ManagedIdentityParams -ErrorAction SilentlyContinue
+
+    }
     # Test the response
     if ($null -eq $response.access_token) { throw "Error. Access token not returned from Azure Instance Metadata Service. Please ensure that the Azure Instance Metadata Service is available." }
 
